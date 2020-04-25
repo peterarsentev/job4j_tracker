37c42f81c7f0069adee33a663b757678bc592549
igorshirokov93 <freelancerigor@yandex.ru>
2020-04-25 17:53:00 +0300

    1.1. Создание объекта [#165452]

---
+++ src/main/java/ru/job4j/oop/Dog.java
@@ -0,0 +1,15 @@
+package ru.job4j.oop;
+
+/**
+ * @author Igor Shirokov (mailto:freelancerigor@yandex.ru)
+ * @version $1$
+ * @since 25.04.2020.
+ */
+
+public class Dog {
+    public static void main(String[] args) {
+        Dog polkan = new Dog();
+        Dog sharik = new Dog();
+        Dog zhychka = new Dog();
+    }
+}
