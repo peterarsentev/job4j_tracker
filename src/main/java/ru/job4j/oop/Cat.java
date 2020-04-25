d3b8c4e4d86ed594e98255a876dd4ff664cff09e
igorshirokov93 <freelancerigor@yandex.ru>
2020-04-25 19:02:02 +0300

    Revise after pull request

--- src/main/java/ru/job4j/oop/Cat.java
+++ src/main/java/ru/job4j/oop/Cat.java
@@ -1,11 +1,5 @@
 package ru.job4j.oop;
 
-/**
- * @author Igor Shirokov (mailto:freelancerigor@yandex.ru)
- * @version $1$
- * @since 25.04.2020.
- */
-
 public class Cat {
     public static void main(String[] args) {
         Cat peppy = new Cat();
