cae61f6a90035a84311450328e247fac9d4a04bd
solodova <a.solodova@adsterratech.com>
2020-05-10 23:12:41 +0300

    1.1.1. Конструктор по умолчанию. [#282766]

--- src/main/java/ru/job4j/oop/Predator.java
+++ src/main/java/ru/job4j/oop/Predator.java
@@ -12,6 +12,6 @@ public class Predator extends Animal {
     }
 
     public static void main(String[] args) {
-        Tiger tiger = new Tiger("jjjj");
+        Tiger tiger = new Tiger();
     }
 }
--- src/main/java/ru/job4j/oop/Tiger.java
+++ src/main/java/ru/job4j/oop/Tiger.java
@@ -1,6 +1,12 @@
 package ru.job4j.oop;
 
 public class Tiger extends Predator {
+
+    public Tiger() {
+        super();
+        System.out.println("Tiger");
+    }
+
     public Tiger(String name){
         super(name);
     }
