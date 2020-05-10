1acdd7a67000e18e5e5374a2614be45cc8ffa984
solodova <a.solodova@adsterratech.com>
2020-05-10 22:31:57 +0300

    5. Аннотация @Override [#282773]

--- src/main/java/ru/job4j/inheritance/HtmlReport.java
+++ src/main/java/ru/job4j/inheritance/HtmlReport.java
@@ -2,6 +2,7 @@ package ru.job4j.inheritance;
 
 public class HtmlReport extends TextReport {
 
+    @Override
     public String generate(String name, String body) {
         return "<h1>" + name + "</h1>" +
                 "<br/>" +
--- src/main/java/ru/job4j/inheritance/JSONReport.java
+++ src/main/java/ru/job4j/inheritance/JSONReport.java
@@ -2,7 +2,7 @@ package ru.job4j.inheritance;
 
 public class JSONReport extends TextReport {
 
-
+    @Override
     public String generate(String name, String body){
         return "{" + "name: " + name + ", " + "body: " + body + "}";
     }
