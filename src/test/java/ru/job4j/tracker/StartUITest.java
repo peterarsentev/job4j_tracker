cae7a5ab693a593f378746261a4162f1b3e0a2ae
asolodova <angel.solodova@gmail.com>
2020-06-14 21:22:37 +0300

    8. Реализация меню за счет шаблона стратегия. [#282724]

--- src/test/java/ru/job4j/tracker/StartUITest.java
+++ src/test/java/ru/job4j/tracker/StartUITest.java
@@ -12,7 +12,8 @@ public class StartUITest {
         String[] answers = {"Fix PC"};
         Input input = new StubInput(answers);
         Tracker tracker = new Tracker();
-        StartUI.createItem(input, tracker);
+        UserAction action = new CreateAction();
+        action.execute(input, tracker);
         Item created = tracker.findAll()[0];
         Item expected = new Item("Fix PC");
         assertThat(created.getName(), is(expected.getName()));
@@ -22,12 +23,13 @@ public class StartUITest {
     public void whenReplaceItem() {
         Tracker tracker = new Tracker();
         Item item = new Item("new item");
+        UserAction action = new ReplaceAction();
         tracker.add(item);
         String[] answers = {
                 item.getId(), // id сохраненной заявки в объект tracker.
                 "replaced item"
         };
-        StartUI.replaceItem(new StubInput(answers), tracker);
+        action.execute(new StubInput(answers), tracker);
         Item replaced = tracker.findById(item.getId());
         assertThat(replaced.getName(), is("replaced item"));
     }
@@ -36,11 +38,12 @@ public class StartUITest {
     public void whenDeleteItem() {
         Tracker tracker = new Tracker();
         Item item = new Item("new item");
+        UserAction action = new DeleteAction();
         tracker.add(item);
         String[] answers = {
                 item.getId(), // id сохраненной заявки в объект tracker.
         };
-        StartUI.deteleItem(new StubInput(answers), tracker);
+        action.execute(new StubInput(answers), tracker);
         assertThat(tracker.findById(item.getId()), is(nullValue()));
     }
 }
