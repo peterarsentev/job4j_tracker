d3c77d6fe90048f94f32608f6945fb60a0fd8cd0
asolodova <angel.solodova@gmail.com>
2020-06-14 20:20:38 +0300

    7. Шаблон проектирования - Стратегия. [#282729]

---
+++ src/main/java/ru/job4j/strategy/Square.java
@@ -0,0 +1,15 @@
+package ru.job4j.strategy;
+
+public class Square implements Shape {
+    @Override
+    public String draw() {
+        String ln = System.lineSeparator();
+        return    "|¯¯¯¯¯¯|" + ln
+                + "|      |" + ln
+                + "|______|" + ln;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(new Square().draw());
+    }
+}
