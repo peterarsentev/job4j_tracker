5b29c5683abb8a429ac344dcaaeda53ed5b0817c
asolodova <angel.solodova@gmail.com>
2020-06-15 22:59:43 +0300

    0.5. Иерархия исключений и множественный catch. [#282738]

--- src/main/java/ru/job4j/ex/UserStore.java
+++ src/main/java/ru/job4j/ex/UserStore.java
@@ -4,38 +4,40 @@ public class UserStore {
 
     public static User findUser(User[] users, String login) throws UserNotFoundException {
         User rsl = null;
-        for (int i = 0; i < users.length; i++) {
-            if (users[i].getUsername().equals(login)) {
-                rsl = users[i];
-            } else {
-                throw new UserNotFoundException("User not found.");
+        for (User user : users) {
+            if (user.getUsername().equals(login)) {
+                rsl = user;
+                break;
             }
+            throw new UserNotFoundException("User not found.");
         }
         return rsl;
     }
 
     public static boolean validate(User user) throws UserInvalidException {
-        if (!user.isValid() || user.getUsername().length() < 3) {
+        if (!user.isValid()) {
             throw new UserInvalidException("Invalid user.");
+        }
+        if (user.getUsername().length() < 3) {
+            throw new UserInvalidException("Username must be greater than 2 symbols");
+
         }
         return true;
     }
 
     public static void main(String[] args) {
         User[] users = {
-                new User("Petr", false)
+                new User("Pe7", true)
         };
-        User user = null;
+        User user;
         try {
-            user = findUser(users, "Petr");
-        } catch (UserNotFoundException e) {
-            e.printStackTrace();
-        }
-        try {
+            user = findUser(users, "Pe7");
             if (validate(user)) {
                 System.out.println("This user has an access");
             }
         } catch (UserInvalidException e) {
+            e.printStackTrace();
+        } catch (UserNotFoundException e) {
             e.printStackTrace();
         }
     }
