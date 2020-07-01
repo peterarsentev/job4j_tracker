1ea0ba13be96f4cb6e88e106f72bf9d92be14635
asolodova <angel.solodova@gmail.com>
2020-07-01 23:36:31 +0300

    1. Телефонный справочник на базе ArrayList [#282695]

---
+++ src/main/java/ru/job4j/search/Person.java
@@ -0,0 +1,53 @@
+package ru.job4j.search;
+
+import java.util.Objects;
+
+public class Person {
+    private String name;
+    private String surname;
+    private String phone;
+    private String address;
+
+    public Person(String name, String surname, String phone, String address) {
+        this.name = name;
+        this.surname = surname;
+        this.phone = phone;
+        this.address = address;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        Person person = (Person) o;
+        return Objects.equals(name, person.name)
+                && Objects.equals(surname, person.surname)
+                && Objects.equals(phone, person.phone)
+                && Objects.equals(address, person.address);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, surname, phone, address);
+    }
+}
---
+++ src/main/java/ru/job4j/search/PhoneDictionary.java
@@ -0,0 +1,30 @@
+package ru.job4j.search;
+
+import java.util.ArrayList;
+
+public class PhoneDictionary {
+    private ArrayList<Person> persons = new ArrayList<>();
+
+    public void add(Person person) {
+        this.persons.add(person);
+    }
+
+    /**
+     * Вернуть список всех пользователей, который содержат key в любых полях.
+     *
+     * @param key Ключ поиска.
+     * @return Список подощедщих пользователей.
+     */
+    public ArrayList<Person> find(String key) {
+        ArrayList<Person> result = new ArrayList<>();
+        for (Person person : persons) {
+            if (person.getName().contains(key) || person.getSurname().contains(key)
+                    || person.getAddress().contains(key) || person.getPhone().contains(key)) {
+                result.add(person);
+            } else {
+                break;
+            }
+        }
+        return result;
+    }
+}
