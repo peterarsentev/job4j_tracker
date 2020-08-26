4fa53fde957381c127cb23b8f06a438e1852ca24
vlader <Vladarscb@gmail.com>
2020-08-26 15:08:06 +1000

    4. Переопределение [#330839]

---
+++ src/main/java/ru/job4j/inheritance/JSONReport.java
@@ -0,0 +1,8 @@
+package ru.job4j.inheritance;
+
+public class JSONReport extends TextReport {
+    @Override
+    public String generate(String name, String body) {
+        return name + " : name" + System.lineSeparator() + body + " : body" ;
+    }
+}
