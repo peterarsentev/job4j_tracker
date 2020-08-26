a7877590f134f4bafa4a2c78a0a665ecdab17bab
vlader <Vladarscb@gmail.com>
2020-08-26 15:37:10 +1000

    добавил скобки и запятую

--- src/main/java/ru/job4j/inheritance/JSONReport.java
+++ src/main/java/ru/job4j/inheritance/JSONReport.java
@@ -3,6 +3,9 @@ package ru.job4j.inheritance;
 public class JSONReport extends TextReport {
     @Override
     public String generate(String name, String body) {
-        return name + " : name" + System.lineSeparator() + body + " : body" ;
+         return "{" + System.lineSeparator() +
+                 name + " : name" + "," + System.lineSeparator() +
+                 body + " : body" + System.lineSeparator() +
+                 "}";
     }
 }
