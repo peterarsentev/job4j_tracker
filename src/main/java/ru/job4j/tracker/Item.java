3bfc33abe12bc8a1369b5e73917204df3b212961
vlader <Vladarscb@gmail.com>
2020-08-26 15:31:42 +1000

    7. toString [#330842]

--- src/main/java/ru/job4j/tracker/Item.java
+++ src/main/java/ru/job4j/tracker/Item.java
@@ -31,4 +31,12 @@ public class Item {
     public void setName(String name) {
         this.name = name;
     }
+
+    @Override
+    public String toString() {
+        return "Item{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                '}';
+    }
 }
---
+++ src/main/java/ru/job4j/tracker/StartUI.java
@@ -0,0 +1,10 @@
+package ru.job4j.tracker;
+
+public class StartUI {
+
+
+    public static void main(String[] args) {
+        Item item = new Item("box", 50);
+        System.out.println(item.toString());
+    }
+}
