79b19a3bb8decf0bdf2320dd60cfa1e3b65b8806
vlader <Vladarscb@gmail.com>
2020-08-28 16:15:34 +1000

    8. Что такое валидация? [#330852]

--- src/main/java/ru/job4j/tracker/Tracker.java
+++ src/main/java/ru/job4j/tracker/Tracker.java
@@ -61,10 +61,10 @@ public class Tracker {
     }
 
     public boolean delete(int id){
-        int index = indexOf(id);
         boolean rsl = false;
+        int index = indexOf(id);
         if (index != -1) {
-            System.arraycopy(items, index +1, items, index, size - index );
+            System.arraycopy(items, index + 1, items, index, size - index);
             items[size - 1] = null;
             size--;
             rsl = true;
