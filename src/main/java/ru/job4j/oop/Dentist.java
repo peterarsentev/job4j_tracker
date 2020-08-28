d941101579e73c4c8cafa1f645630df068b2a052
vlader <Vladarscb@gmail.com>
2020-08-28 15:18:47 +1000

    остальное

--- src/main/java/ru/job4j/oop/Dentist.java
+++
@@ -1,7 +0,0 @@
-package ru.job4j.oop;
-
-public class Dentist extends Doctor {
-    public boolean hasTeeth(Man man) {
-
-    }
-}
--- src/main/java/ru/job4j/oop/Surgeon.java
+++
@@ -1,7 +0,0 @@
-package ru.job4j.oop;
-
-public class Surgeon extends Doctor {
-    public void cutLeg(Patient man) {
-
-    }
-}
