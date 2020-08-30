e600e21779ea21f904916e0c3bdf099b2ee79851
vlader <Vladarscb@gmail.com>
2020-08-30 20:55:46 +1000

    5.1. Тестирование. Подготовка данных. [#330783]

--- src/test/java/ru/job4j/tracker/StartUITest.java
+++ src/test/java/ru/job4j/tracker/StartUITest.java
@@ -7,14 +7,30 @@ import static org.junit.Assert.*;
 
 public class StartUITest {
     @Test
-    public void whenAddItem() {
-        String[] answers = {"Fix PC"};
-        Input input = new StubInput(answers);
+    public void whenReplaceItem() {
         Tracker tracker = new Tracker();
-        StartUI.createItem(input, tracker);
-        Item created = tracker.findAll()[0];
-        Item expected = new Item("Fix PC", 2);
-        assertThat(created.getName(), is(expected.getName()));
+        Item item = new Item("new item");
+        tracker.add(item);
+        String[] answers = {
+                //item.getId(), // id сохраненной заявки в объект tracker.
+                "replaced item"
+        };
+        StartUI.replaceItem(new StubInput(answers), tracker);
+        Item replaced = tracker.findById(item.getId());
+        assertThat(replaced.getName(), is("replaced item"));
+    }
+
+    @Test
+    public void whenDeleteItem() {
+        Tracker tracker = new Tracker();
+        Item item = new Item("new item");
+        tracker.add(item);
+        String[] answers = {
+                "new item"
+        };
+        StartUI.deleteItem(new StubInput(answers), tracker);
+        Item[] exp = tracker.findByName("new item");
+        assertThat(exp[0], null );
     }
 }
 
