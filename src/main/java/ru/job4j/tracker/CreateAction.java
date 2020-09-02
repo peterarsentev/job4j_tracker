6b4991b25ca3df95f8cdfa0e31ff9e0ac8e7003a
vlader <Vladarscb@gmail.com>
2020-09-02 17:05:09 +1000

    9.1 Зависимость от System.out [#330772]

--- src/main/java/ru/job4j/tracker/CreateAction.java
+++ src/main/java/ru/job4j/tracker/CreateAction.java
@@ -1,13 +1,20 @@
 package ru.job4j.tracker;
 
 public class CreateAction implements UserAction {
+    private final Output out;
+
+    public CreateAction(Output out) {
+        this.out = out;
+    }
+
     @Override
     public String name() {
-        return "=== Create a new Item ====";
+        return "Create";
     }
 
     @Override
     public boolean execute(Input input, Tracker tracker) {
+        out.println("=== Create a new Item ====");
         String name = input.askStr("Enter name: ");
         Item item = new Item(name);
         tracker.add(item);
