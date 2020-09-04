36811ab8803e8a3add9690cf3e4aa74abecd488e
vlader <Vladarscb@gmail.com>
2020-09-04 12:57:36 +1000

    0.4. Пользовательские исключения. [#330796]

---
+++ src/main/java/ru/job4j/ex/ElementNotFoundException.java
@@ -0,0 +1,7 @@
+package ru.job4j.ex;
+
+public class ElementNotFoundException extends Exception {
+    ElementNotFoundException(String sign) {
+        super(sign);
+    }
+}
---
+++ src/main/java/ru/job4j/ex/FindEl.java
@@ -0,0 +1,25 @@
+package ru.job4j.ex;
+
+public class FindEl {
+    public static int indexOf(String[] value, String key) throws ElementNotFoundException {
+        int rsl = -1;
+        for (int i = 0; i < value.length; i++) {
+            if (value[i].equals(key)) {
+                rsl = i;
+            }
+        }
+        if (rsl == -1) {
+            throw new ElementNotFoundException("not found");
+        }
+        return rsl;
+    }
+
+    public static void main(String[] args) {
+        String[] mas = {"not key"};
+        try {
+            int result = indexOf(mas, "key");
+        } catch (ElementNotFoundException ex) {
+            ex.printStackTrace();
+        }
+    }
+}
