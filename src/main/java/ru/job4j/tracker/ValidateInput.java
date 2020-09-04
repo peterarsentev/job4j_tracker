cb38b2506b9b04a34c31b24a13862e0ef439c6be
vlader <Vladarscb@gmail.com>
2020-09-04 18:53:49 +1000

    1. Обеспечить бесперебойную работу приложения Tracker [#330801]

---
+++ src/main/java/ru/job4j/tracker/ValidateInput.java
@@ -0,0 +1,19 @@
+package ru.job4j.tracker;
+
+public class ValidateInput extends ConsoleInput {
+
+    @Override
+    public int askInt(String question) {
+        boolean invalid = true;
+        int value = -1;
+        do {
+            try {
+                value = super.askInt(question);
+                invalid = false;
+            } catch (NumberFormatException nfe) {
+                System.out.println("Please enter validate data again.");
+            }
+        } while (invalid);
+        return value;
+    }
+}
