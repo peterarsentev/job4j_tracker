befa7b76229047e3320ba0875d3f02ee81c0d43f
vlader <Vladarscb@gmail.com>
2020-09-04 18:57:33 +1000

    1.1. Тесты на StartUI. [#330799]

--- src/test/java/ru/job4j/tracker/StartUITest.java
+++ src/test/java/ru/job4j/tracker/StartUITest.java
@@ -76,53 +76,27 @@ public class StartUITest {
         ));
     }
 
-/*    @Test
-    public void whenCreateItem() {
-        Input in = new StubInput(
-                new String[] {"0", "Item name", "1" }
-        );
-        Tracker tracker = new Tracker();
-        UserAction[] actions = {
-                new CreateAction(),
-                new Exit()
-        };
-        new StartUI().init(in, tracker, actions);
-        assertThat(tracker.findById(1).getName(), is("Item name"));
-    }
-
     @Test
-    public void whenReplaceItem() {
-        Tracker tracker = new Tracker();
-         Добавим в tracker новую заявку
-        Item item = tracker.add(new Item("Replaced item"));
-         Входные данные должны содержать ID добавленной заявки item.getId()
-        String replacedName = "New item name";
+    public void whenInvalidExit() {
+        Output out = new StubOutput();
         Input in = new StubInput(
-                new String[] {"0", valueOf(item.getId()), replacedName, "1"}
+                new String[] { "-1", "0" }
         );
+        Tracker tracker = new Tracker();
         UserAction[] actions = {
-                new ReplaceAction(),
-                new Exit()
+                new Exit(out)
         };
-        new StartUI().init(in, tracker, actions);
-        assertThat(tracker.findById(item.getId()).getName(), is(replacedName));
+        new StartUI(out).init(in, tracker, actions);
+        assertThat(out.toString(), is(
+                String.format(
+                        "Menu.%n"
+                                + "0. Exit%n"
+                                + "Wrong input, you can select: 0 .. 0%n"
+                                + "Menu.%n"
+                                + "0. Exit%n"
+                                + "== Exit! ==%n"
+                )
+        ));
     }
-
-    @Test
-    public void whenDeleteItem() {
-        Tracker tracker = new Tracker();
-         Добавим в tracker новую заявку
-        Item item = tracker.add(new Item("Deleted item"));
-         Входные данные должны содержать ID добавленной заявки item.getId()
-        Input in = new StubInput(
-                new String[] {"0", valueOf(item.getId()), "1"}
-        );
-        UserAction[] actions = {
-                new DeleteAction(),
-                new Exit()
-        };
-        new StartUI().init(in, tracker, actions);
-        assertThat(tracker.findById(item.getId()), is(nullValue()));
-    }*/
 }
 
