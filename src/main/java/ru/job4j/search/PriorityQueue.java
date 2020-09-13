bd45fbe2c73e79350d258343391305af180a4f77
vlader <Vladarscb@gmail.com>
2020-09-13 17:47:07 +1000

    >= -> >

--- src/main/java/ru/job4j/search/PriorityQueue.java
+++ src/main/java/ru/job4j/search/PriorityQueue.java
@@ -14,7 +14,7 @@ public class PriorityQueue {
     public void put(Task task) {
         int index = 0;
         for (Task element : tasks) {
-            if (element.getPriority() >= task.getPriority()) {
+            if (element.getPriority() > task.getPriority()) {
                 break;
             }
             index++;
