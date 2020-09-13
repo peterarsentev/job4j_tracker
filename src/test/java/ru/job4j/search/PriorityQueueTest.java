f39ccef2994e76721064cfcd5483d0a75fe0a851
vlader <Vladarscb@gmail.com>
2020-09-13 14:51:24 +1000

    2. Очередь с приоритетом на LinkedList [#330754]

---
+++ src/test/java/ru/job4j/search/PriorityQueueTest.java
@@ -0,0 +1,22 @@
+package ru.job4j.search;
+
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+public class PriorityQueueTest {
+    @Test
+    public void whenHigherPriority() {
+        PriorityQueue queue = new PriorityQueue();
+        queue.put(new Task("low", 5));
+        queue.put(new Task("urgent", 1));
+        queue.put(new Task("middle", 3));
+        Task result = queue.take();
+        assertThat(result.getDesc(), is("urgent"));
+    }
+}
