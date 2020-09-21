f30936ac28435b7bd3d39425c1e6f16a72109ef2
vlader <Vladarscb@gmail.com>
2020-09-21 12:50:33 +1000

    2.2. Лямбда блок [#330593]

--- src/main/java/ru/job4j/lambda/Stroki.java
+++ src/main/java/ru/job4j/lambda/Stroki.java
@@ -6,7 +6,9 @@ public class Stroki {
     public static void main(String[] args) {
         Comparator<String> cmpText = (left, right) ->
                 left.compareTo(right);
-        Comparator<String> cmpDescSize = (left, right) ->
-                Integer.compare(left.length(), right.length());
+        Comparator<String> cmpDescSize = (left, right) -> {
+            System.out.println("compare - " + left.length() + " : " + right.length());
+            return Integer.compare(left.length(), right.length());
+        };
     }
 }
