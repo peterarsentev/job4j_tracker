45b9d4eff872eb8722a20f4d782e31875f38fb35
vlader <Vladarscb@gmail.com>
2020-09-21 12:44:33 +1000

    2.1. Лямбда [#330588]

---
+++ src/main/java/ru/job4j/lambda/Stroki.java
@@ -0,0 +1,12 @@
+package ru.job4j.lambda;
+
+import java.util.Comparator;
+
+public class Stroki {
+    public static void main(String[] args) {
+        Comparator<String> cmpText = (left, right) ->
+                left.compareTo(right);
+        Comparator<String> cmpDescSize = (left, right) ->
+                Integer.compare(left.length(), right.length());
+    }
+}
