3d5dae82a18df8e6b95b458d579420f77515a3fd
vlader <Vladarscb@gmail.com>
2020-09-22 15:36:50 +1000

    fix Stroki

--- src/main/java/ru/job4j/lambda/Stroki.java
+++ src/main/java/ru/job4j/lambda/Stroki.java
@@ -8,7 +8,7 @@ public class Stroki {
                 left.compareTo(right);
         Comparator<String> cmpDescSize = (left, right) -> {
             System.out.println("compare - " + left.length() + " : " + right.length());
-            return Integer.compare(left.length(), right.length());
+            return Integer.compare(right.length(), left.length());
         };
     }
 }
