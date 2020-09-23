fec0381aaa3cd93be8a8b3b9a44f220a5a7bb410
vlader <Vladarscb@gmail.com>
2020-09-23 16:59:07 +1000

    2. Список адресов [#330602]

---
+++ src/main/java/ru/job4j/stream/Address.java
@@ -0,0 +1,8 @@
+package ru.job4j.stream;
+
+public class Address {
+    private String city;
+    private String street;
+    private int home;
+    private int apartment;
+}
---
+++ src/main/java/ru/job4j/stream/Profile.java
@@ -0,0 +1,9 @@
+package ru.job4j.stream;
+
+public class Profile {
+    private Address address;
+
+    public Address getAddress() {
+        return address;
+    }
+}
