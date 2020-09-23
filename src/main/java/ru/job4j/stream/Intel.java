24d25c59447afa6a7e7e4eb5a804282e5152e2bc
vlader <Vladarscb@gmail.com>
2020-09-23 16:27:45 +1000

    0. Stream API [#330600]

---
+++ src/main/java/ru/job4j/stream/Intel.java
@@ -0,0 +1,14 @@
+package ru.job4j.stream;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class Intel {
+    public static void main(String[] args) {
+        List<Integer> listik = new ArrayList<Integer>();
+        listik = List.of(1, 50, 4, 6, 3, 8, 15, -5, -18);
+        List<Integer> positive = listik.stream().filter(el -> el > 0).collect(Collectors.toList());
+        positive.forEach(System.out::println);
+    }
+}
