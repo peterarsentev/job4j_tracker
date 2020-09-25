7a9e6771ce868fc219b9a668502d08bca0bd6416
vlader <Vladarscb@gmail.com>
2020-09-25 18:17:11 +1000

    fix tests for Profiles method

---
+++ src/test/java/ru/job4j/stream/ProfilesTest.java
@@ -0,0 +1,32 @@
+package ru.job4j.stream;
+
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.Assert.*;
+
+public class ProfilesTest {
+
+    @Test
+    public void dis() {
+        List<Profile> add = List.of(new Profile(new Address("Adler")),
+                new Profile(new Address("Sochi")),
+                new Profile(new Address("Adler")));
+        List<Address> exp = List.of(new Address("Adler"),
+                new Address("Sochi"));
+        assertEquals(exp, Profiles.collect(add));
+    }
+
+    @Test
+    public void sort() {
+        List<Profile> add = List.of(new Profile(new Address("Moscow")),
+                new Profile(new Address("Sochi")),
+                new Profile(new Address("Adler")));
+        List<Address> exp = List.of(new Address("Adler"),
+                new Address("Moscow"),
+                new Address("Sochi"));
+        assertEquals(exp, Profiles.collect(add));
+    }
+}
