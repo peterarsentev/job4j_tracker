be45b4fe9be6608256a9901a64f2efa3b8fa4b94
vlader <Vladarscb@gmail.com>
2020-09-29 19:56:32 +1000

    4. Optional в Stream API. [#330615] fixed

--- src/main/java/ru/job4j/bank/BankService.java
+++ src/main/java/ru/job4j/bank/BankService.java
@@ -27,30 +27,26 @@ public class BankService {
                 .findFirst();
     }
 
-
-    public Account findByRequisite(String passport, String requisite) {
+    public Optional<Account> findByRequisite(String passport, String requisite) {
         Account rsl = null;
         Optional<User> user = findByPassport(passport);
-        if (user.isPresent()) {
-            return users.get(user.get()).stream()
-                     .filter(e -> e
-                             .getRequisite()
-                             .equals(requisite))
-                     .findFirst()
-                    .orElse(null);
-        }
-        return null;
+        return user.flatMap(value -> users.get(value).stream()
+                .filter(e -> e
+                        .getRequisite()
+                        .equals(requisite))
+                .findFirst());
     }
 
     public boolean transferMoney(String srcPassport, String srcRequisite,
                                  String destPassport, String destRequisite, double amount) {
         boolean rsl = false;
         Optional<User> found = findByPassport(srcPassport);
-        Account account = findByRequisite(srcPassport, srcRequisite);
-        Account accountDest = findByRequisite(destPassport, destRequisite);
-        if ((found.isPresent()) && (account != null) && (account.getBalance() >= amount)) {
-            account.setBalance(account.getBalance() - amount);
-            accountDest.setBalance(accountDest.getBalance() + amount);
+        Optional<Account> account = findByRequisite(srcPassport, srcRequisite);
+        Optional<Account> accountDest = findByRequisite(destPassport, destRequisite);
+        if ((found.isPresent()) && (accountDest.isPresent())
+                && (account.isPresent()) && (account.get().getBalance() >= amount)) {
+            account.get().setBalance(account.get().getBalance() - amount);
+            accountDest.get().setBalance(accountDest.get().getBalance() + amount);
             rsl = true;
         }
         return rsl;
