25d71fdc49cd07fc740cb7ee1512583f283a1a0a
Kostyantyn Chyzhyk <kostyantyn.chyzhyk@marconirovereto.it>
2020-10-03 14:56:47 +0200

    2. Модель данных.[174683#266896]

---
+++ src/main/java/ru/job4j/oop/Student.java
@@ -0,0 +1,32 @@
+package ru.job4j.oop;
+
+public class Student{
+    private String name;
+    private String group;
+    private String date;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getGroup() {
+        return group;
+    }
+
+    public void setGroup(String group) {
+        this.group = group;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+}
