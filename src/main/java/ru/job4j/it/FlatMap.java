4fcf1892f819b4a5212cd7fa5dc633a7011fd28c
vlader <Vladarscb@gmail.com>
2020-10-15 21:19:53 +1000

    flat fix 2

--- src/main/java/ru/job4j/it/FlatMap.java
+++ src/main/java/ru/job4j/it/FlatMap.java
@@ -1,5 +1,6 @@
 package ru.job4j.it;
 
+import java.util.Collections;
 import java.util.Iterator;
 import java.util.List;
 import java.util.NoSuchElementException;
@@ -10,9 +11,7 @@ public class FlatMap<T> implements Iterator<T> {
 
     public FlatMap(Iterator<Iterator<T>> data) {
         this.data = data;
-        if (data.hasNext()) {
-            cursor = data.next();
-        }
+        cursor = Collections.emptyIterator();
     }
 
     @Override
