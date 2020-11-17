764dd2745213015127d3ca24228c4f48a9e10474
vlader <Vladarscb@gmail.com>
2020-11-17 15:44:28 +1000

    3. Переопределить только hashCode [#381599]

--- src/main/java/ru/job4j/hash/User.java
+++ src/main/java/ru/job4j/hash/User.java
@@ -3,11 +3,16 @@ package ru.job4j.hash;
 import java.util.Calendar;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Objects;
 
 public class User {
     private String name;
     private int children;
     private Calendar birthday;
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, children);
+    }
 
     public static void main(String[] args) {
         User user1 = new User();
