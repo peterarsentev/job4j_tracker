f0f4bb4ad02e10b7defc4e3f44dfecda9db757f5
Satrim <59724286+Satrim@users.noreply.github.com>
2020-12-13 22:31:08 +0300

    1.1. Создание объекта [#409210]

---
+++ src/main/java/ru/job4j/oop/Cat.java
@@ -0,0 +1,9 @@
+package ru.job4j.oop;
+
+public class Cat {
+    public static void main(String[] args) {
+        Cat polkan = new Cat();
+        Cat sharik = new Cat();
+        Cat zhychka = new Cat();
+    }
+}
