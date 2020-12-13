0f10ffc2dbec4a598e359d91525c343fed68c777
Satrim <59724286+Satrim@users.noreply.github.com>
2020-12-13 23:29:16 +0300

    1.5. Поля объекта [#409211]

--- src/main/java/ru/job4j/oop/Cat.java
+++ src/main/java/ru/job4j/oop/Cat.java
@@ -1,9 +1,38 @@
 package ru.job4j.oop;
 
 public class Cat {
+
+    private String food;
+    private String name;
+
+    public void show() {
+        System.out.println(this.food);
+        System.out.println(this.name);
+    }
+
+    public void eat(String meat) {
+        this.food = meat;
+    }
+
+    public void giveNick(String nick) {
+        this.name = nick;
+    }
+
     public static void main(String[] args) {
         Cat polkan = new Cat();
         Cat sharik = new Cat();
         Cat zhychka = new Cat();
+
+        //1.5. Поля объекта [#409211]
+        System.out.println("There are gav's food.");
+        Cat gav = new Cat();
+        gav.eat("kotleta");
+        gav.giveNick("Barsik");
+        gav.show();
+        System.out.println("There are black's food.");
+        Cat black = new Cat();
+        black.eat("fish");
+        black.giveNick("Mursik");
+        black.show();
     }
 }
