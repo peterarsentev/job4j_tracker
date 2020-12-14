e4b6b05419bda1e6ce62a965e6c9c04f834da179
Satrim <59724286+Satrim@users.noreply.github.com>
2020-12-14 15:06:07 +0300

    1.1. Создание объекта [#409210]

---
+++ src/main/java/ru/job4j/oop/Dog.java
@@ -0,0 +1,9 @@
+package ru.job4j.oop;
+
+public class Dog {
+    public static void main(String[] args) {
+        Cat polkan = new Cat();
+        Cat sharik = new Cat();
+        Cat zhychka = new Cat();
+    }
+}
