8a4d4ce0bd1f4f086ec8505015c30c2dbde24831
Satrim <59724286+Satrim@users.noreply.github.com>
2020-12-24 16:03:32 +0300

    9. Написать тесты на StartUI. [#409172]

--- src/test/java/ru/job4j/tracker/TrackerTest.java
+++ src/test/java/ru/job4j/tracker/TrackerTest.java
@@ -38,52 +38,10 @@ public class TrackerTest {
         assertThat(tracker.findById(id), is(nullValue()));
     }
 
-//    @Test
-//    public void whenAddItem() {
-//        String[] answers = {"Fix PC"};
-//        Input input = new StubInput(answers);
-//        Tracker tracker = new Tracker();
-//        CreateAction createAction = new CreateAction();
-//        createAction.execute(input, tracker);
-//        Item created = tracker.findAll()[0];
-//        Item expected = new Item("Fix PC");
-//        assertThat(created.getName(), is(expected.getName()));
-//    }
-
-//    @Test
-//    public void whenReplaceItem() {
-//        Tracker tracker = new Tracker();
-//        Item item = new Item("new item");
-//        tracker.add(item);
-//        String[] answer = {
-//                String.valueOf(item.getId()), "replaced item"
-//        };
-////        StartUI.editItem(new StubInput(answer), tracker);
-//        EditAction editAction = new EditAction();
-//        editAction.execute(new StubInput(answer), tracker);
-//        Item replaced = tracker.findById(item.getId());
-//        assertThat(replaced.getName(), is("replaced item"));
-//    }
-
-//    @Test
-//    public void whenDeleteItem() {
-//        Tracker tracker = new Tracker();
-//        Item item = new Item("new item");
-//        tracker.add(item);
-//        String[] answer = {
-//                String.valueOf(item.getId())
-//        };
-////        StartUI.deleteItem(new StubInput(answer), tracker);
-//        DeleteAction deleteAction = new DeleteAction();
-//        deleteAction.execute(new StubInput(answer), tracker);
-//        Item deleted = tracker.findById(item.getId());
-//        Assert.assertNull(deleted);
-//    }
-
     @Test
     public void whenCreateItem() {
         Input in = new StubInput(
-                new String[] {"0", "Item name", "1"}
+                new String[]{"0", "Item name", "1"}
         );
         Tracker tracker = new Tracker();
         UserAction[] actions = {
@@ -101,7 +59,7 @@ public class TrackerTest {
         /* Входные данные должны содержать ID добавленной заявки item.getId() */
         String replacedName = "New item name";
         Input in = new StubInput(
-                new String[] {"0" , "1", replacedName, "1"}
+                new String[]{"0", "1", replacedName, "1"}
         );
         UserAction[] actions = {
                 new EditAction(),
@@ -118,7 +76,7 @@ public class TrackerTest {
         Item item = tracker.add(new Item("Deleted item"));
         /* Входные данные должны содержать ID добавленной заявки item.getId() */
         Input in = new StubInput(
-                new String[] {"0", "1", "1"}
+                new String[]{"0", "1", "1"}
         );
         UserAction[] actions = {
                 new DeleteAction(),
