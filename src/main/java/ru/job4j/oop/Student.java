a650b5d3130f4007e53c38a3880099038e5617f4
tutbora <tutbora@gmail.com>
2020-12-25 17:26:36 +0300

    temp Student

--- src/main/java/ru/job4j/oop/Student.java
+++ src/main/java/ru/job4j/oop/Student.java
@@ -2,12 +2,17 @@ package ru.job4j.oop;
 
 public class Student {
 
-    public void song() {
-        System.out.println("I believe I can fly");
+    public String song() {
+        System.out.println("I believe, I can fly");
+        return "I believe, I can fly";
     }
 
     public void music() {
         System.out.println("Tra tra tra");
+    }
+
+    public void music(String lyrics) {
+        System.out.println("I can sign a song : " + lyrics);
     }
 
     public static void main(String[] args) {
@@ -18,5 +23,8 @@ public class Student {
         petya.song();
         petya.song();
         petya.song();
+        String song = "I believe, I can fly";
+        petya.music(song);
+        petya.music(petya.song());
     }
 }
