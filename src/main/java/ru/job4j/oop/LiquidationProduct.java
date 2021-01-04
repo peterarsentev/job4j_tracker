f9ef4ef74edab97d11d3459fcb3d6e903e466693
tutbora <tutbora@gmail.com>
2021-01-04 23:27:39 +0300

    6. Почему наследование - это плохо [#425006]

---
+++ src/main/java/ru/job4j/oop/LiquidationProduct.java
@@ -0,0 +1,13 @@
+package ru.job4j.oop;
+
+public final class LiquidationProduct {
+    private Product product;
+
+    public LiquidationProduct(String name, int price) {
+        product = new Product(name, price);
+    }
+
+    public String label() {
+        return product.label();
+    }
+}
