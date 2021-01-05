805ca8b1ff22af38917f1c152f47b4f6dfc8f50e
tutbora <tutbora@gmail.com>
2021-01-05 11:53:29 +0300

    7. Date. Отображение даты.!исправлено [#425007]

--- src/main/java/ru/job4j/tracker/StartUI.java
+++ src/main/java/ru/job4j/tracker/StartUI.java
@@ -7,7 +7,6 @@ public class StartUI {
         Item time = new Item();
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MMMM-EEEE-yyyy HH:mm:ss");
         String currentDateTimeFormat = time.getCreated().format(formatter);
-        //String currentDateTimeFormat = time.created.format(formatter); // какой вариант правильный, этот или верхний?
         System.out.println("Текущие дата и время после форматирования: " + currentDateTimeFormat);
 
     }
