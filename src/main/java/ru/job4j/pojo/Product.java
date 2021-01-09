84fbdedd65004683895e573d86a6a56ad7a41e48
tutbora <tutbora@gmail.com>
2021-01-09 21:47:14 +0300

    4. Сравнение моделей. Метод equals [#425013]

--- src/main/java/ru/job4j/pojo/Product.java
+++ src/main/java/ru/job4j/pojo/Product.java
@@ -1,8 +1,27 @@
 package ru.job4j.pojo;
 
+import java.util.Objects;
+
 public class Product {
     private String name;
     private int count;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        Product product = (Product) o;
+        return count == product.count && Objects.equals(name, product.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, count);
+    }
 
     public Product(String name, int count) {
         this.name = name;
---
+++ src/test/java/ru/job4j/pojo/LicenseTest.java
@@ -0,0 +1,17 @@
+package ru.job4j.pojo;
+
+import static org.junit.Assert.assertThat;
+import static org.hamcrest.Matchers.is;
+import org.junit.Test;
+
+public class LicenseTest {
+
+    @Test
+    public void eqName() {
+        License first = new License();
+        first.setCode("audio");
+        License second = new License();
+        second.setCode("audio");
+        assertThat(first, is(second));
+    }
+}
