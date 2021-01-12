9b773e1c9268571ae07634052b88691b7c792e31
tutbora <tutbora@gmail.com>
2021-01-12 10:32:48 +0300

    7. Метод удаления заявки Tracker.delete !исправлено [#425016]

--- src/main/java/ru/job4j/tracker/Tracker.java
+++ src/main/java/ru/job4j/tracker/Tracker.java
@@ -64,8 +64,7 @@ public class Tracker {
             items[size - 1] = null;
             size--;
             return true;
-        } else {
-            return false;
         }
+            return false;
     }
 }
