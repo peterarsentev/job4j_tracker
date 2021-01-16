546bec1fda6d9607de8a48c567a3c4a049899c9a
Alexander Slepchenko <asander@list.ru>
2021-01-16 17:13:22 +0300

    1. Паспорт и жители [#409147]

---
+++ src/main/java/ru/job4j/collection/PassportOffice.java
@@ -0,0 +1,23 @@
+package ru.job4j.collection;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class PassportOffice {
+    private Map<String, Citizen> citizens = new HashMap<>();
+
+    public boolean add(Citizen citizen) {
+        boolean rsl = false;
+        if (!citizens.containsKey(citizen.getPassport())) {
+            citizens.put(citizen.getPassport(), citizen);
+            rsl = true;
+        }
+        return rsl;
+    }
+
+
+    public Citizen get(String passport) {
+        Citizen citizen = citizens.get(passport);
+        return citizen;
+    }
+}
