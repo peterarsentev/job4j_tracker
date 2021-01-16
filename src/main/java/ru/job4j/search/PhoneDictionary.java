54d36d6460bb1db9bced1101e625a05805d5a54f
asolodova <angel.solodova@gmail.com>
2021-01-16 18:07:35 +0300

    1. Телефонный справочник на базе ArrayList [#282695]

--- src/main/java/ru/job4j/search/PhoneDictionary.java
+++ src/main/java/ru/job4j/search/PhoneDictionary.java
@@ -21,8 +21,6 @@ public class PhoneDictionary {
             if (person.getName().contains(key) || person.getSurname().contains(key)
                     || person.getAddress().contains(key) || person.getPhone().contains(key)) {
                 result.add(person);
-            } else {
-                break;
             }
         }
         return result;
