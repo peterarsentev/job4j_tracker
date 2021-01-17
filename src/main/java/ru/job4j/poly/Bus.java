44fbe3aafa6fd33d073101a2b27ed2a9356463d7
tutbora <tutbora@gmail.com>
2021-01-17 17:57:20 +0300

    3. Интерфейс Input [#424942]

---
+++ src/main/java/ru/job4j/poly/Bus.java
@@ -0,0 +1,16 @@
+package ru.job4j.poly;
+
+public class Bus implements Transport {
+    @Override
+    public void goBy() {
+    }
+
+    @Override
+    public void passengers(int number) {
+    }
+
+    @Override
+    public double refuelPrice(double litresOfFuel) {
+        return 0;
+    }
+}
