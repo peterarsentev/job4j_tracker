e8bd492c1cdeb1f5286c33236ab87d94b01b5855
Alexander Slepchenko <asander@list.ru>
2021-01-22 20:01:31 +0300

    4. Сортировка номера [#409133]

--- src/main/java/ru/job4j/collection/LexSort.java
+++ src/main/java/ru/job4j/collection/LexSort.java
@@ -5,32 +5,14 @@ import java.util.Comparator;
 public class LexSort implements Comparator<String> {
     @Override
     public int compare(String left, String right) {
-        String leftNum = "";
-        String rightNum = "";
-        for (int i = 0; i < Math.min(left.length(), right.length()); i++) {
-            if (left.charAt(i) > 47 && left.charAt(i) < 58) {
-                leftNum += String.valueOf(left.charAt(i));
-            }
-            if (left.charAt(i) == 46) {
-                leftNum += " ";
-            }
-            if (right.charAt(i) > 47 && right.charAt(i) < 58) {
-                rightNum += String.valueOf(right.charAt(i));
-            }
-            if (right.charAt(i) == 46) {
-                rightNum += " ";
-            }
+        String[] leftNumbers = left.split("\\.");
+        String[] rightNumbers = right.split("\\.");
+
+        if (Integer.parseInt(leftNumbers[0]) > Integer.parseInt(rightNumbers[0])) {
+            return 1;
         }
-
-        String[] leftNumbers = leftNum.split(" ");
-        String[] rightNumbers = rightNum.split(" ");
-
-         for(int i = 0; i < Math.min(leftNumbers.length, rightNumbers.length); i++) {
-            if (Integer.parseInt(leftNumbers[i]) > Integer.parseInt(rightNumbers[i])) {
-                return 1;
-            } else if (Integer.parseInt(leftNumbers[i]) < Integer.parseInt(rightNumbers[i])) {
-                return -1;
-            }
+        if (Integer.parseInt(leftNumbers[0]) < Integer.parseInt(rightNumbers[0])) {
+            return -1;
         }
         return 0;
     }
