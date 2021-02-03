a64df36c5102ea967a53fac5ca41415874c75988
Alexander Slepchenko <asander@list.ru>
2021-02-03 21:15:21 +0300

    1. Экзамен. Коллекции. [#409130]

--- src/main/java/ru/job4j/collection/Article.java
+++ src/main/java/ru/job4j/collection/Article.java
@@ -1,26 +1,21 @@
 package ru.job4j.collection;
 
-import java.util.Arrays;
 import java.util.HashSet;
-import java.util.Iterator;
 import java.util.Set;
 
 public class Article {
     public static boolean generateBy(String origin, String line) {
-
         String[] orig = origin.split(" ");
         String[] lines = line.split(" ");
 
         Set<String> originSet = new HashSet<>();
-        Set<String> lineSet = new HashSet<>(Arrays.asList(lines));
 
-        for (int i = 0; i < orig.length; i++) {
-            originSet.add(orig[i].replaceAll("[.,:;!?()]", ""));
+        for (String s : orig) {
+            originSet.add(s.replaceAll("\\p{P}", ""));
         }
 
-        Iterator<String> iterator = lineSet.iterator();
-        while (iterator.hasNext()) {
-            if (originSet.add(iterator.next())) {
+        for (String l : lines) {
+            if (!originSet.contains(l)) {
                 return false;
             }
         }
