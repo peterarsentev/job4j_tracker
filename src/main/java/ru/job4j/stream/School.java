ccad43f74cc59f7d45e685d9e619fa4af1b641bc
Alexander Slepchenko <asander@list.ru>
2021-02-17 22:21:19 +0300

    1. Фильтрация учеников [#408971]

---
+++ src/main/java/ru/job4j/stream/School.java
@@ -0,0 +1,14 @@
+package ru.job4j.stream;
+
+import java.util.List;
+import java.util.function.Predicate;
+import java.util.stream.Collectors;
+
+public class School {
+    public List<Student> collect(List<Student> students, Predicate<Student> predict) {
+        List<Student> result = students.stream()
+                .filter(predict)
+                .collect(Collectors.toList());
+        return result;
+    }
+}
