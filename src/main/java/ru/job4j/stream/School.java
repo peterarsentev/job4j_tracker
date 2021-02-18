159c5ac8b21855fd851e802e30e7c6d7eee64371
Satrim <59724286+Satrim@users.noreply.github.com>
2021-02-18 17:15:46 +0300

    1. Фильтрация учеников [#408971]

--- src/main/java/ru/job4j/stream/School.java
+++ src/main/java/ru/job4j/stream/School.java
@@ -6,9 +6,9 @@ import java.util.stream.Collectors;
 
 public class School {
     public List<Student> collect(List<Student> students, Predicate<Student> predict) {
-        List<Student> result = students.stream()
+//        List<Student> result =
+        return students.stream()
                 .filter(predict)
                 .collect(Collectors.toList());
-        return result;
     }
 }
