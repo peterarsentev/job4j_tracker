b33cf8bb3f3543b151103971b4f7d36cb1865cc3
Satrim <59724286+Satrim@users.noreply.github.com>
2021-02-19 22:41:49 +0300

    4. Преобразование List в Map. [#408974]

--- src/main/java/ru/job4j/stream/School.java
+++ src/main/java/ru/job4j/stream/School.java
@@ -1,14 +1,18 @@
 package ru.job4j.stream;
 
 import java.util.List;
+import java.util.Map;
 import java.util.function.Predicate;
 import java.util.stream.Collectors;
 
 public class School {
     public List<Student> collect(List<Student> students, Predicate<Student> predict) {
-//        List<Student> result =
         return students.stream()
                 .filter(predict)
                 .collect(Collectors.toList());
+    }
+
+    public Map<String, Student> mapCollect(List<Student> students) {
+        return students.stream().distinct().collect(Collectors.toMap(e -> e.getSurname(), e -> e));
     }
 }
--- src/test/java/ru/job4j/stream/SchoolTest.java
+++ src/test/java/ru/job4j/stream/SchoolTest.java
@@ -4,7 +4,9 @@ import org.junit.Before;
 import org.junit.Test;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.function.Predicate;
 
 import static org.hamcrest.core.Is.is;
@@ -59,6 +61,24 @@ public class SchoolTest {
         expected.add(new Student(20, "Surname2"));
         expected.add(new Student(30, "Surname3"));
         expected.add(new Student(40, "Surname4"));
+        assertThat(rsl, is(expected));
+    }
+
+    @Test
+    public void whenCollectToMap() {
+        School sc = new School();
+        Map<String, Student> rsl = sc.mapCollect(students);
+        Map<String, Student> expected = new HashMap<>();
+        expected.put("Surname1", new Student(10, "Surname1"));
+        expected.put("Surname2", new Student(20, "Surname2"));
+        expected.put("Surname3", new Student(30, "Surname3"));
+        expected.put("Surname4", new Student(40, "Surname4"));
+        expected.put("Surname5", new Student(50, "Surname5"));
+        expected.put("Surname6", new Student(60, "Surname6"));
+        expected.put("Surname7", new Student(70, "Surname7"));
+        expected.put("Surname8", new Student(80, "Surname8"));
+        expected.put("Surname9", new Student(90, "Surname9"));
+
         assertThat(rsl, is(expected));
     }
 }
