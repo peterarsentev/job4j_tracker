aaba9ff832fb29bbc06d7e9606ecd58c29e5b524
Satrim <59724286+Satrim@users.noreply.github.com>
2021-02-20 23:39:19 +0300

    8. Перенос методов в Stream API [#408978]

--- src/main/java/ru/job4j/stream/Deck.java
+++ src/main/java/ru/job4j/stream/Deck.java
@@ -5,14 +5,8 @@ import java.util.stream.Stream;
 public class Deck {
 
     public static void main(String[] args) {
-        Suit suit = Suit.Clubs;
-        Value value = Value.V_6;
-        Card card = new Card(suit, value);
-
-        Stream.of(Suit.values())
-                .flatMap(s -> Stream.of(Value.values())
-                .map(s -> s. ))
+        Stream.of(Value.values())
+                .flatMap(v -> Stream.of(Suit.values())
+                .map(s -> v + " " + s)).forEach(System.out::println);
     }
-
-
 }
