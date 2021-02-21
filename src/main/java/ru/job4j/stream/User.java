57f68c5704ad709eff7eaf08546200473c6d713a
Satrim <59724286+Satrim@users.noreply.github.com>
2021-02-22 00:05:41 +0300

    9. Шаблоны проектирования в Stream API [#408979]

---
+++ src/main/java/ru/job4j/stream/User.java
@@ -0,0 +1,88 @@
+package ru.job4j.stream;
+
+public class User {
+    private String name;
+
+    private String surname;
+
+    private byte age;
+
+    private String login;
+
+    private String password;
+
+    private boolean activated;
+
+    private String gender;
+
+
+
+    public static void main(String[] args) {
+        User user = new Builder().buildName("name")
+                .buildSurname("surname")
+                .buildAge((byte) 32)
+                .buildLogin("login")
+                .buildPassword("password")
+                .buildActivated(true)
+                .buildGender("male")
+                .build();
+        System.out.println(user);
+    }
+
+    static class Builder {
+        private String name;
+        private String surname;
+        private byte age;
+        private String login;
+        private String password;
+        private boolean activated;
+        private String gender;
+
+        Builder buildName(String name) {
+            this.name = name;
+            return this;
+        }
+
+        Builder buildSurname(String surname) {
+            this.surname = surname;
+            return this;
+        }
+
+        Builder buildAge(byte age) {
+            this.age = age;
+            return this;
+        }
+
+        Builder buildLogin(String login) {
+            this.login = login;
+            return this;
+        }
+
+        Builder buildPassword(String password) {
+            this.password = password;
+            return this;
+        }
+
+        Builder buildActivated(boolean activated) {
+            this.activated = activated;
+            return this;
+        }
+
+        Builder buildGender(String gender) {
+            this.gender = gender;
+            return this;
+        }
+
+        public User build() {
+            User user = new User();
+            user.name = name;
+            user.surname = surname;
+            user.age = age;
+            user.login = login;
+            user.password = password;
+            user.activated = activated;
+            user.gender = gender;
+            return user;
+        }
+    }
+}
