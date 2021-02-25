c97e35fb82a53b377e1e5e535b415b6a8bb4598c
demonick82 <demonick82@gmail.com>
2021-02-26 00:39:48 +0300

    5. Аннотация @Override [#469741]

--- src/main/java/ru/job4j/inheritance/JSONReport.java
+++ src/main/java/ru/job4j/inheritance/JSONReport.java
@@ -2,7 +2,7 @@ package ru.job4j.inheritance;
 
 public class JSONReport extends TextReport {
 
-    @SuppressWarnings("checkstyle:OperatorWrap")
+    @Override
     public String generate(String name, String body) {
         return "{" + System.lineSeparator()
                 + "\"name\" : \"" + name + "\"," + System.lineSeparator()
