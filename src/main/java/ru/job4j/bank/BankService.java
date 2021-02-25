2b9a38718163cccff3a24dd2580a6defd764c609
Alexander Slepchenko <asander@list.ru>
2021-02-25 23:24:46 +0300

    4. Optional в Stream API. [#408985]

--- src/main/java/ru/job4j/bank/BankService.java
+++ src/main/java/ru/job4j/bank/BankService.java
@@ -1,13 +1,11 @@
 package ru.job4j.bank;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 /**
  * Класс описывает работу банковской базы данных пользователей и списоков аккаунтов
  * пользователей
+ *
  * @author job4j.ru
  * @version 1.0
  */
@@ -22,6 +20,7 @@ public class BankService {
      * Метод добавляет в коллекцию HashMap пользователя как ключ и создает список
      * аккаунтов пользователя
      * ArrayList, добавляет его как значение.
+     *
      * @param user пользователь, который добавляется в хранилище
      */
     public void addUser(User user) {
@@ -31,77 +30,76 @@ public class BankService {
     /**
      * Метод находит пользователя по данным паспорта, добавляет в список аккаунтов
      * пользователя новый аккаунт
+     *
      * @param passport значение переменной по данным которой производится поиск
      *                 пользователя
-     * @param account аккаунт, который добавляется в список аккаунта найденного
-     *                пользователя
+     * @param account  аккаунт, который добавляется в список аккаунта найденного
+     *                 пользователя
      */
     public void addAccount(String passport, Account account) {
-        User user = findByPassport(passport);
-        if (user != null) {
-            if (!users.get(user).contains(account)) {
-                users.get(user).add(account);
+        Optional<User> user = findByPassport(passport);
+        if (user.isPresent()) {
+            if (!users.get(user.get()).contains(account)) {
+                users.get(user.get()).add(account);
             }
         }
     }
 
     /**
      * Метод производит поиск пользователя по данным паспорта
+     *
      * @param passport значение переменной по данным которой производится поиск пользователя
      * @return возвращает найденного пользователя
      */
-    public User findByPassport(String passport) {
+    public Optional<User> findByPassport(String passport) {
         return users.keySet()
                 .stream()
                 .filter(p -> p.getPassport().equals(passport))
-                .findFirst()
-                .orElse(null);
+                .findFirst();
     }
 
     /**
      * Метод производит поиск пользователя в хранилище по данным паспорта,
      * а так же производит поиск аккаунта по реквизитам
-     * @param passport значение переменной по данным которой производится
-     *                 поиск пользователя
+     *
+     * @param passport  значение переменной по данным которой производится
+     *                  поиск пользователя
      * @param requisite значение переменной по данным которой производится
      *                  поиск аккаунта найденного пользователя
      * @return возвращает найденный аккаунт пользователя
      */
-    public Account findByRequisite(String passport, String requisite) {
-        User user = findByPassport(passport);
-        if (user != null) {
-            return users.get(user)
-                    .stream()
-                    .filter(a -> a.getRequisite().equals(requisite))
-                    .findFirst()
-                    .orElse(null);
-        }
-        return null;
+    public Optional<Account> findByRequisite(String passport, String requisite) {
+        Optional<User> user = findByPassport(passport);
+        return user.flatMap(value -> users.get(value)
+                .stream()
+                .filter(a -> a.getRequisite().equals(requisite))
+                .findFirst());
+
     }
 
     /**
      * Метод производит трансфер финансовых средств из аккаунта одного пользователя
      * в аккаунт другого.
-     * @param srcPassport значение переменной по данным которой производится поиск
-     *                    пользователя отправителя
-     * @param srcRequisite значение переменной по данным которой производится поиск
-     *                     аккаунта отправителя
-     * @param destPassport значение переменной по данным которой производится поиск
-     *                     пользователя получателя
+     *
+     * @param srcPassport   значение переменной по данным которой производится поиск
+     *                      пользователя отправителя
+     * @param srcRequisite  значение переменной по данным которой производится поиск
+     *                      аккаунта отправителя
+     * @param destPassport  значение переменной по данным которой производится поиск
+     *                      пользователя получателя
      * @param destRequisite значение переменной по данным которой производится поиск
      *                      аккаунта получателя
-     * @param amount величина передаваемых денежных средств
+     * @param amount        величина передаваемых денежных средств
      * @return возвращается результат работы метода
      */
     public boolean transferMoney(String srcPassport, String srcRequisite,
                                  String destPassport, String destRequisite, double amount) {
-        Account srcAcc = findByRequisite(srcPassport, srcRequisite);
-        Account destAcc = findByRequisite(destPassport, destRequisite);
-        if (srcAcc != null && destAcc != null && srcAcc.getBalance() >= amount) {
-            findByRequisite(destPassport, destRequisite).setBalance(destAcc.getBalance() + amount);
-            findByRequisite(srcPassport, srcRequisite).setBalance(srcAcc.getBalance() - amount);
+        Optional<Account> srcAcc = findByRequisite(srcPassport, srcRequisite);
+        Optional<Account> destAcc = findByRequisite(destPassport, destRequisite);
+        if (srcAcc.isPresent() && destAcc.isPresent() && srcAcc.get().getBalance() >= amount) {
+            findByRequisite(destPassport, destRequisite).get().setBalance(destAcc.get().getBalance() + amount);
+            findByRequisite(srcPassport, srcRequisite).get().setBalance(srcAcc.get().getBalance() - amount);
             return true;
-
         }
         return false;
     }
--- src/test/java/ru/job4j/bank/BankServiceTest.java
+++ src/test/java/ru/job4j/bank/BankServiceTest.java
@@ -2,6 +2,8 @@ package ru.job4j.bank;
 
 import org.junit.Test;
 
+import java.util.Optional;
+
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.*;
 
@@ -12,7 +14,7 @@ public class BankServiceTest {
         User user = new User("3434");
         BankService bank = new BankService();
         bank.addUser(user);
-        assertThat(bank.findByPassport("3434"), is(user));
+        assertThat(bank.findByPassport("3434").get(), is(user));
     }
 
     @Test
@@ -21,7 +23,8 @@ public class BankServiceTest {
         BankService bank = new BankService();
         bank.addUser(user);
         bank.addAccount(user.getPassport(), new Account("5546", 150D));
-        assertNull(bank.findByRequisite("34", "5546"));
+        Optional<Account> expected = Optional.empty();
+        assertNull(bank.findByRequisite("34", "5546").get());
     }
 
     @Test
@@ -30,7 +33,7 @@ public class BankServiceTest {
         BankService bank = new BankService();
         bank.addUser(user);
         bank.addAccount(user.getPassport(), new Account("5546", 150D));
-        assertThat(bank.findByRequisite("3434", "5546").getBalance(), is(150D));
+        assertThat(bank.findByRequisite("3434", "5546").get().getBalance(), is(150D));
     }
 
     @Test
@@ -41,6 +44,6 @@ public class BankServiceTest {
         bank.addAccount(user.getPassport(), new Account("5546", 150D));
         bank.addAccount(user.getPassport(), new Account("113", 50D));
         bank.transferMoney(user.getPassport(), "5546", user.getPassport(), "113", 150D);
-        assertThat(bank.findByRequisite(user.getPassport(), "113").getBalance(), is(200D));
+        assertThat(bank.findByRequisite(user.getPassport(), "113").get().getBalance(), is(200D));
     }
 }
