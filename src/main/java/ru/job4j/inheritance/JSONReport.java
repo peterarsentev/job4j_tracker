ee7799309297c5944af304ff2c8c0be7fc4112a2
demonick82 <demonick82@gmail.com>
2021-02-26 21:19:26 +0300

    6. Почему наследование - это плохо [#469742]

--- src/main/java/ru/job4j/inheritance/JSONReport.java
+++ src/main/java/ru/job4j/inheritance/JSONReport.java
@@ -1,7 +1,7 @@
 package ru.job4j.inheritance;
 
 public class JSONReport extends TextReport {
-
+    @Override
     public String generate(String name, String body) {
         return "{" + System.lineSeparator()
                 + "\"name\" : \"" + name + "\"," + System.lineSeparator()
