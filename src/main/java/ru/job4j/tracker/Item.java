64787a9bff05457a1f6b0315362416ba1bd38f01
demonick82 <demonick82@gmail.com>
2021-02-26 21:34:33 +0300

    7. Date. Отображение даты. [#469743]

--- src/main/java/ru/job4j/tracker/Item.java
+++ src/main/java/ru/job4j/tracker/Item.java
@@ -1,8 +1,11 @@
 package ru.job4j.tracker;
 
+import java.time.LocalDateTime;
+
 public class Item {
     private int id;
     private String name;
+    private LocalDateTime created = LocalDateTime.now();
 
     public int getId() {
         return id;
@@ -18,5 +21,9 @@ public class Item {
 
     public void setName(String name) {
         this.name = name;
+    }
+
+    public LocalDateTime getCreated() {
+        return created;
     }
 }
