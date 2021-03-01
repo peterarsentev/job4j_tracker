64775561ada2414fc0cf89b2ae351e7e69fbaa0c
demonick82 <demonick82@gmail.com>
2021-03-02 00:42:56 +0300

    3. Интерфейс Input [#469678]

---
+++ src/main/java/ru/job4j/poly/Bus.java
@@ -0,0 +1,20 @@
+package ru.job4j.poly;
+
+public class Bus implements Transport {
+
+    @Override
+    public void drive() {
+
+    }
+
+    @Override
+    public void passengers(int passengers) {
+
+    }
+
+    @Override
+    public double load(double amount) {
+        double price = 0;
+        return price;
+    }
+}
