7dab8b1d00162caf822e5acb4321c250c7622b5a
demonick82 <demonick82@gmail.com>
2021-03-05 00:54:48 +0300

    9.1 Зависимость от System.out [#469674]

--- src/main/java/ru/job4j/tracker/StartUI.java
+++ src/main/java/ru/job4j/tracker/StartUI.java
@@ -19,9 +19,9 @@ public class StartUI {
 
     private void showMenu(UserAction[] actions) {
 
-        System.out.println("Меню.");
+        out.println("Меню.");
         for (int index = 0; index < actions.length; index++) {
-            System.out.println(index + ". " + actions[index].name());
+            out.println(index + ". " + actions[index].name());
         }
     }
 
