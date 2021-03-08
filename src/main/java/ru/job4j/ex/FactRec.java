983b79defc3c2bdcc0bef58745f85b33d9319880
demonick82 <demonick82@gmail.com>
2021-03-08 15:41:20 +0300

    0.6. Error - исключения, связанные с работой виртуальной машины [#469700]

--- src/main/java/ru/job4j/ex/FactRec.java
+++ src/main/java/ru/job4j/ex/FactRec.java
@@ -4,9 +4,8 @@ public class FactRec {
     public static int calc(int n) {
         if (n == 0 || n == 1) {
             return 1;
-        } else {
-            return calc(n - 1) * n;
         }
+        return calc(n - 1) * n;
     }
 
     public static void main(String[] args) {
