673b8d16760ba949f4aeab5d4140c4145a611070
demonick82 <demonick82@gmail.com>
2021-03-15 02:19:02 +0300

    1. Паспорт и жители [#469664]

---
+++ src/main/java/ru/job4j/colllection/PassportOffice.java
@@ -0,0 +1,28 @@
+package ru.job4j.colllection;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class PassportOffice {
+    private Map<String, Citizen> citizens = new HashMap<>();
+
+    public boolean add(Citizen citizen) {
+        boolean rsl = false;
+        if (!citizens.containsKey(citizen)) {
+            citizens.put(citizen.getPassport(), citizen);
+            return true;
+        }
+        return rsl;
+    }
+
+    public Citizen get(String passport) {
+        Citizen citizen;
+        for (String s : citizens.keySet()) {
+            citizen = citizens.get(s);
+            if (citizen.getPassport().equals(passport)) {
+                return citizen;
+            }
+        }
+        return null;
+    }
+}
