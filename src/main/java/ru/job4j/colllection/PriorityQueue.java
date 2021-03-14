a1e1da7d4f15820433459c8dd2b45155c458e41e
demonick82 <demonick82@gmail.com>
2021-03-14 03:47:11 +0300

    2. Очередь с приоритетом на LinkedList [#469656]

---
+++ src/main/java/ru/job4j/colllection/PriorityQueue.java
@@ -0,0 +1,24 @@
+package ru.job4j.colllection;
+
+import ru.job4j.search.Task;
+
+import java.util.LinkedList;
+
+public class PriorityQueue {
+    private LinkedList<ru.job4j.search.Task> tasks = new LinkedList<>();
+
+    public void put(ru.job4j.search.Task task) {
+        int index = 0;
+        for (ru.job4j.search.Task element : tasks) {
+            if (element.getPriority() < task.getPriority()) {
+                index++;
+                break;
+            }
+        }
+        this.tasks.add(index, task);
+    }
+
+    public Task take() {
+        return tasks.poll();
+    }
+}
--- src/test/java/ru/job4j/search/PriorityQueueTest.java
+++ src/test/java/ru/job4j/search/PriorityQueueTest.java
@@ -1,6 +1,7 @@
 package ru.job4j.search;
 
 import org.junit.Test;
+import ru.job4j.colllection.PriorityQueue;
 
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertThat;
