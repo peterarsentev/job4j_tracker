f46515a00e8764ce7880017dc6d31e826ff42787
demonick82 <demonick82@gmail.com>
2021-03-28 01:16:35 +0300

    2. Список адресов [#469489]

--- src/main/java/ru/job4j/stream/Profiles.java
+++ src/main/java/ru/job4j/stream/Profiles.java
@@ -1,16 +1,12 @@
 package ru.job4j.stream;
 
-import java.util.Comparator;
 import java.util.List;
 import java.util.stream.Collectors;
 
 public class Profiles {
     public List<Address> collect(List<Profile> profiles) {
-        Comparator<Address> comp = (left, right) -> left.getCity().compareTo(right.getCity());
         return profiles.stream()
                 .map(p -> p.getAddress())
-                .sorted(comp)
-                .distinct()
                 .collect(Collectors.toList());
     }
 }
