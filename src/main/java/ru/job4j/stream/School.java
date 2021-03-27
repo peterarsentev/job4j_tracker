b88bdf086e454cead3731c77acd536256947a534
demonick82 <demonick82@gmail.com>
2021-03-28 01:09:09 +0300

    1. Фильтрация учеников [#469488]

--- src/main/java/ru/job4j/stream/School.java
+++ src/main/java/ru/job4j/stream/School.java
@@ -6,8 +6,8 @@ import java.util.stream.Collectors;
 
 public class School {
     public List<Student> collect(List<Student> students, Predicate<Student> predict) {
-        List<Student> collect = students.stream()
-                .filter(predict).collect(Collectors.toList());
-        return collect;
+        return students.stream()
+                .filter(predict)
+                .collect(Collectors.toList());
     }
 }
