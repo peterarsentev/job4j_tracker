80de1dd508a5df9a274d02eeaf6ffc9e680d64de
Aleks <no>
2021-04-11 20:32:30 +0300

    1.1 Создание объекта

---
+++ src/test/java/ru/job4j/tracker/oop/Dog.java
@@ -0,0 +1,16 @@
+package ru.job4j.tracker.oop;
+
+public class Dog {
+
+    private String  name;
+
+    public static void main(String[] args) {
+
+        Dog polkan = new Dog();
+        Dog sharik = new Dog();
+        Dog zhychka = new Dog();
+
+
+
+    }
+}
