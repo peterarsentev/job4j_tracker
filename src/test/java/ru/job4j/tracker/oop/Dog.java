c36f18245a5aaa3fad95f3b5eaa80465051c1382
Aleks <no>
2021-04-12 22:15:30 +0300

    new version

--- src/test/java/ru/job4j/tracker/oop/Dog.java
+++ src/test/java/ru/job4j/tracker/oop/Dog.java
@@ -1,16 +1,10 @@
 package ru.job4j.tracker.oop;
 
 public class Dog {
-
-    private String  name;
-
     public static void main(String[] args) {
-
         Dog polkan = new Dog();
         Dog sharik = new Dog();
         Dog zhychka = new Dog();
-
-
 
     }
 }
