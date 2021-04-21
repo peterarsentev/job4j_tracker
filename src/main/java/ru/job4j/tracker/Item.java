18172a46ddc294345a2f94eb12f265a38702c094
Aleks <no>
2021-04-21 20:05:38 +0300

    2. Перегрузка конструктора

--- src/main/java/ru/job4j/tracker/Item.java
+++ src/main/java/ru/job4j/tracker/Item.java
@@ -4,6 +4,17 @@ public class Item {
     private int id;
     private String name;
 
+    public Item(int id) {
+        this.id = id;
+    }
+    public Item(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+    public Item() {
+
+    }
+
     public int getId() {
         return id;
     }
