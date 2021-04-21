782f96ca6a2f2567c2a91ef6d83fd37cb487ab10
Aleks <no>
2021-04-21 22:39:06 +0300

    Реализация профессий в коде

---
+++ src/main/java/other/Doctor.java
@@ -0,0 +1,13 @@
+package other;
+
+public class Doctor extends Profession {
+
+    public Doctor(String surName, String name, String education, int birthday) {
+        super(surName, name, education, birthday);
+    }
+
+    public Diagnosis heal(Pacient pacient) {
+
+        return d;
+    }
+}
---
+++ src/main/java/other/Engineer.java
@@ -0,0 +1,8 @@
+package other;
+
+public class Engineer extends Profession {
+
+    public Engineer(String surName, String name, String education, int birthday) {
+        super(surName, name, education, birthday);
+    }
+}
