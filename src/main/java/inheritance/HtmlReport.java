af50ac91d329347ab3f4c073587e2d230cdc5f87
Aleks <no>
2021-04-22 22:55:19 +0300

    Переопределение

---
+++ src/main/java/inheritance/HtmlReport.java
@@ -0,0 +1,11 @@
+package inheritance;
+
+public class HtmlReport extends TextReport {
+    public String generate(String name, String body) {
+        return "<h1>" + name + "</h1>" +
+                "<br/>" +
+                "<span>" + body + "</span>";
+    }
+
+
+}
