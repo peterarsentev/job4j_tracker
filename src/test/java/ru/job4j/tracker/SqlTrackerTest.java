3394d9869ef463d5bb8dc043cf837de497aa978d
demonick82 <demonick82@gmail.com>
2021-06-09 22:31:56 +0300

    2. Трекер SQL [#1734] (Исправления)

---
+++ src/test/java/ru/job4j/tracker/SqlTrackerTest.java
@@ -0,0 +1,47 @@
+package ru.job4j.tracker;
+
+import org.junit.Test;
+
+import java.io.InputStream;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.util.Properties;
+
+import static org.hamcrest.Matchers.nullValue;
+import static org.junit.Assert.*;
+import static org.hamcrest.core.Is.is;
+
+
+public class SqlTrackerTest {
+    public Connection init() {
+        try (InputStream in = SqlTracker.class.getClassLoader().getResourceAsStream("app.properties")) {
+            Properties config = new Properties();
+            config.load(in);
+            Class.forName(config.getProperty("driver-class-name"));
+            return DriverManager.getConnection(
+                    config.getProperty("url"),
+                    config.getProperty("username"),
+                    config.getProperty("password")
+            );
+        } catch (Exception e) {
+            throw new IllegalStateException(e);
+        }
+    }
+
+    @Test
+    public void createItem() throws Exception {
+        try (SqlTracker tracker = new SqlTracker(ConnectionRollback.create(this.init()))) {
+            tracker.add(new Item("name"));
+            assertThat(tracker.findByName("name").size(), is(1));
+        }
+    }
+    @Test
+    public void deleteItem() throws Exception {
+        try (SqlTracker tracker = new SqlTracker(ConnectionRollback.create(this.init()))) {
+            tracker.add(new Item("name"));
+            tracker.delete(62);
+            assertThat(tracker.findByName("name"), is(nullValue()));
+        }
+    }
+
+}
