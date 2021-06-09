7ece225c1616cb31e9b84b81d9d86f298c87664b
demonick82 <demonick82@gmail.com>
2021-06-09 22:33:28 +0300

    2. Трекер SQL [#1734] (Исправления)

--- src/main/java/ru/job4j/tracker/SqlTracker.java
+++ src/main/java/ru/job4j/tracker/SqlTracker.java
@@ -14,6 +14,9 @@ public class SqlTracker implements Store {
         init();
     }
 
+    public SqlTracker(Connection cn) {
+        this.cn = cn;
+    }
     public void init() {
         try (InputStream in = SqlTracker.class.getClassLoader().
                 getResourceAsStream("app.properties")) {
