3a1aad20262dff845a6ab11db28581bd7e683f7c
MbPro13_chudikO <oleg.chudnovskiy.gmail.com>
2021-07-05 13:27:14 +0300

    1.8. Состояние объекта [#167388]

---
+++ src/main/java/ru/job4j/oop/ru/job4j/oop/Battery.java
@@ -0,0 +1,33 @@
+package ru.job4j.oop.ru.job4j.oop;
+
+public class Battery {
+    private int load;
+
+    public Battery(int load) {
+        this.load = load;
+    }
+
+    public void exchange(Battery another) {
+        another.load += this.load;
+        this.load = 0;
+        System.out.println("Charging");
+    }
+
+    @Override
+    public String toString() {
+        return "Battery{" + "load =" + load + '}';
+    }
+
+    public static void main(String[] args) {
+        Battery batterySource = new Battery(10);
+        Battery batteryDestination = new Battery(2);
+        getDataFromBattary(batterySource);
+        getDataFromBattary(batteryDestination);
+        batterySource.exchange(batteryDestination);
+        getDataFromBattary(batterySource);
+        getDataFromBattary(batteryDestination);    }
+
+    private static void getDataFromBattary(Battery battery) {
+        System.out.println(battery);
+    }
+}
