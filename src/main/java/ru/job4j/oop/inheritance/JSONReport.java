a8a46eb969cc753944c61d78edf859d2dbc05dbe
MbPro13_chudikO <oleg.chudnovskiy.gmail.com>
2021-07-06 11:48:10 +0300

    4. Переопределение [#174890 #174641] refactoring

--- src/main/java/ru/job4j/oop/inheritance/JSONReport.java
+++ src/main/java/ru/job4j/oop/inheritance/JSONReport.java
@@ -3,8 +3,8 @@ package ru.job4j.oop.inheritance;
 public class JSONReport extends TextReport {
     @Override
     public String generate(String name, String body) {
-        final String nameStr = "{" + System.lineSeparator() + "\t\"name\" : " + "\"" + name + "\",";
-        final String bodyStr = "\t\"body\": " + "\"" + body + "\"" + System.lineSeparator() + "}";
-        return nameStr + System.lineSeparator() + bodyStr;
+        return "{" + System.lineSeparator() + "\t\"name\" : " + "\"" + name + "\","
+                + System.lineSeparator() + "\t\"body\": " + "\""
+                + body + "\"" + System.lineSeparator() + "}";
     }
 }
