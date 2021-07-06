bdae5a92d8332adbdd34a70882b42a8642f32a11
MbPro13_chudikO <oleg.chudnovskiy.gmail.com>
2021-07-06 07:05:04 +0300

    5. Аннотация @Override [#174891]

--- src/main/java/ru/job4j/oop/inheritance/JSONReport.java
+++ src/main/java/ru/job4j/oop/inheritance/JSONReport.java
@@ -1,6 +1,7 @@
 package ru.job4j.oop.inheritance;
 
 public class JSONReport extends TextReport {
+    @Override
     public String generate(String name, String body) {
         final String nameStr = "{" + System.lineSeparator() + "\t\"name\" : " + "\"" + name + "\",";
         final String bodyStr = "\t\"body\": " + "\"" + body + "\"" + System.lineSeparator() + "}";
