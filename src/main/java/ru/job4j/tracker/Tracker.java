041678d1e3a17fed9bd89bafc126b1d7a1cc8a05
MbPro13_chudikO <oleg.chudnovskiy.gmail.com>
2021-07-07 10:29:05 +0300

    5. Tracker - хранилище [#396 #149766]

--- src/main/java/ru/job4j/tracker/Tracker.java
+++ src/main/java/ru/job4j/tracker/Tracker.java
@@ -1,16 +1,22 @@
 package ru.job4j.tracker;
 
+import java.awt.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class Tracker {
     private final Item[] items = new Item[100];
     private int ids = 1;
     private int size = 0;
 
+    //добавление заявок
     public Item add(Item item) {
         item.setId(ids++);
         items[size++] = item;
         return item;
     }
 
+    //получение заявки по id
     public Item findById(int id) {
         Item rsl = null;
         for (int index = 0; index < size; index++) {
@@ -22,4 +28,26 @@ public class Tracker {
         }
         return rsl;
     }
+
+    //получение списка всех заявок
+     public Item[] findAll() {
+         ArrayList<Item> itemsList = new ArrayList<>();
+         for (Item item: items) {
+             if (item != null && item.getName() != null) {
+                 itemsList.add(item);
+             }
+         }
+         return itemsList.toArray(new Item[0]);
+    }
+
+     //получение списка по имени
+     public Item[] findByName(String key) {
+        ArrayList<Item> itemsList = new ArrayList<>();
+         for (int index = 0; index < size; index++) {
+             if (key.equals(items[index].getName())) {
+                itemsList.add(items[index]);
+             }
+         }
+         return itemsList.toArray(new Item[0]);
+     }
 }
