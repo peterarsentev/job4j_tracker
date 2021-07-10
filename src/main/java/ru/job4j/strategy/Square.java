8145e94dce49c1ab9540d8b08fdb80a1359086c8
MbPro13_chudikO <oleg.chudnovskiy.gmail.com>
2021-07-10 17:35:58 +0300

    7. Шаблон проектирования - Стратегия [#786 #149756]

---
+++ src/main/java/ru/job4j/strategy/Square.java
@@ -0,0 +1,25 @@
+package ru.job4j.strategy;
+
+public class Square implements Shape {
+    @Override
+    public String draw() {
+        String ln = System.lineSeparator();
+        StringBuilder square = new StringBuilder();
+        final int SIZE_RECT = 5;
+        for (int j = 0; j < SIZE_RECT; j++) {
+            for (int i = 0; i < SIZE_RECT; i++) {
+                if (j == 0 || j == SIZE_RECT - 1 || i == 0 || i == SIZE_RECT - 1) {
+                    square.append('*');
+                } else {
+                    square.append(" ");
+                }
+            }
+            square.append(ln);
+        }
+        return square.toString();
+    }
+
+    public static void main(String[] args) {
+        System.out.println(new Triangle().draw());
+    }
+}
