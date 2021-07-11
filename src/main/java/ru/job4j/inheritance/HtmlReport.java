f290d080a92ad4c3f8da2a15aad1883a1de5cb87
shabelnikilya <shabelnik_ilya@mail.ru>
2021-07-11 14:48:38 +0300

    5. Аннотация @override [#174891]

---
+++ src/main/java/ru/job4j/inheritance/HtmlReport.java
@@ -0,0 +1,11 @@
+package ru.job4j.inheritance;
+
+public class HtmlReport extends TextReport {
+
+    @Override
+    public String generate(String name, String body) {
+        return "<h1>" + name + "</h1>" +
+                "<br/>" +
+                "<span>" + body + "</span>";
+    }
+}
--- src/main/java/ru/job4j/inheritance/JSONReport.java
+++ src/main/java/ru/job4j/inheritance/JSONReport.java
@@ -4,7 +4,6 @@ public class JSONReport extends TextReport{
 
     @Override
     public String generate(String name, String body) {
-        return "{" + System.lineSeparator() + "name : " + name + "," +
-                System.lineSeparator() + "body : " + body + System.lineSeparator() + "}";
+        return name + ": name" + System.lineSeparator() + body + ": body";
     }
 }
