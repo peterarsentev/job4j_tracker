d1a9394f282f315b5b5126f4d60754046cff7dfe
shabelnikilya <shabelnik_ilya@mail.ru>
2021-07-11 13:52:52 +0300

    1.2. Вызов метода объекта [#166085 #174622]

---
+++ src/main/java/ru/job4j/oop/Student.java
@@ -0,0 +1,21 @@
+package ru.job4j.oop;
+
+public class Student {
+    public void music() {
+        System.out.println("Tra tra tra");
+    }
+
+    public void song() {
+        System.out.println("I believe i can fly");
+    }
+
+    public static void main(String[] args) {
+        Student petya = new Student();
+        petya.music();
+        petya.music();
+        petya.music();
+        petya.song();
+        petya.song();
+        petya.song();
+    }
+}
