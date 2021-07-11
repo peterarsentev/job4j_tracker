4b61107511d9d633ae9e04f9177a2fc5fd131c10
shabelnikilya <shabelnik_ilya@mail.ru>
2021-07-11 17:04:42 +0300

    5. Tracker - хранилище [#396]

--- src/main/java/ru/job4j/tracker/Tracker.java
+++ src/main/java/ru/job4j/tracker/Tracker.java
@@ -1,5 +1,7 @@
 package ru.job4j.tracker;
 
+import java.util.Arrays;
+
 public class Tracker {
     private final Item[] items = new Item[100];
     private int ids = 1;
@@ -9,6 +11,34 @@ public class Tracker {
         item.setId(ids++);
         items[size++] = item;
         return item;
+    }
+
+    public Item[] findAll() {
+        Item[] itemWithOutNull = new Item[size];
+        int number = 0;
+        for (int index = 0; index < size; index++) {
+            Item completed = items[index];
+            if (completed != null) {
+                itemWithOutNull[number] = completed;
+                number++;
+            }
+        }
+        itemWithOutNull = Arrays.copyOf(itemWithOutNull, number);
+        return itemWithOutNull;
+    }
+
+    public Item[] findByName(String kay) {
+        Item[] searchName = new Item[size];
+        int number = 0;
+        for (int index = 0; index < size; index++) {
+            Item coincide = items[index];
+            if (coincide.getName().equals(kay)) {
+                searchName[number] = items[index];
+                number++;
+            }
+        }
+        searchName = Arrays.copyOf(searchName, number);
+        return searchName;
     }
 
     public Item findById(int id) {
