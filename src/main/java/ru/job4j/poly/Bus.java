3859fb1d45965aeec496e0f44d61b38c10476816
shabelnikilya <shabelnik_ilya@mail.ru>
2021-07-13 18:51:48 +0300

    3. Интерфейс Input [#181092]

---
+++ src/main/java/ru/job4j/poly/Bus.java
@@ -0,0 +1,18 @@
+package ru.job4j.poly;
+
+public class Bus implements Transport{
+    @Override
+    public void drive() {
+
+    }
+
+    @Override
+    public void passengers(int numberPassengers) {
+
+    }
+
+    @Override
+    public int fillUp(int fuel) {
+        return 0;
+    }
+}
---
+++ src/main/java/ru/job4j/poly/Transport.java
@@ -0,0 +1,10 @@
+package ru.job4j.poly;
+
+public interface Transport {
+        void drive();
+
+        void passengers(int numberPassengers);
+
+        int fillUp(int fuel);
+
+}
