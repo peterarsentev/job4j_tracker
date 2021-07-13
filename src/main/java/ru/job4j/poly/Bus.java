0c81a7fbcbf8e5b06c0ffc2d3a63c5c7b6a2c273
shabelnikilya <shabelnik_ilya@mail.ru>
2021-07-13 19:50:10 +0300

    3. Интерфейс Input [#181092 #177311] - Изменения

--- src/main/java/ru/job4j/poly/Bus.java
+++ src/main/java/ru/job4j/poly/Bus.java
@@ -3,16 +3,18 @@ package ru.job4j.poly;
 public class Bus implements Transport{
     @Override
     public void drive() {
-
+        System.out.println("Количество километров - 876.");
     }
 
     @Override
     public void passengers(int numberPassengers) {
+        System.out.println("Всего пассажиров " + numberPassengers);
 
     }
 
     @Override
     public int fillUp(int fuel) {
-        return 0;
+        int price = 49;
+        return fuel * price;
     }
 }
