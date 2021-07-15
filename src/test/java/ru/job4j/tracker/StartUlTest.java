67d6a04fba46d3c64249d88deb2a6fa6c1de49aa
shabelnikilya <shabelnik_ilya@mail.ru>
2021-07-15 18:56:17 +0300

    5.1. Тестирование. Подготовка данных. [#182960]

--- src/test/java/ru/job4j/tracker/StartUlTest.java
+++ src/test/java/ru/job4j/tracker/StartUlTest.java
@@ -1,8 +1,10 @@
 package ru.job4j.tracker;
 
+import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.nullValue;
 import org.junit.Test;
 import static org.junit.Assert.*;
-import static org.hamcrest.core.Is.is;
+
 
 public class StartUlTest {
     @Test
@@ -14,5 +16,32 @@ public class StartUlTest {
         Item created = tracker.findAll()[0];
         Item expected = new Item("Fix PC");
         assertThat(created.getName(), is(expected.getName()));
+    }
+
+    @Test
+    public void whenReplaceItem() {
+        Tracker tracker = new Tracker();
+        Item item = new Item();
+        tracker.add(item);
+        String[] answers = {
+                String.valueOf(item.getId()),
+                "replaced item"
+        };
+        StartUl.EditItem(tracker, new StubInput(answers));
+        Item replaced = tracker.findById(item.getId());
+        assertThat(replaced.getName(), is("replaced item"));
+    }
+
+    @Test
+    public void whenDeleteItem() {
+        Tracker tracker = new Tracker();
+        Item item = new Item();
+        tracker.add(item);
+        String[] answers = {
+                String.valueOf(item.getId())
+        };
+        StartUl.deleteItem(tracker, new StubInput(answers));
+        Item replaced = tracker.findById(item.getId());
+        assertThat(replaced, is(nullValue()));
     }
 }
