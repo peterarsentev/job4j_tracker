a29aa11f1d1af28ebf2fdddfcb9bbfe5ca2eb176
chydikO_x220 <oleg.chudnovskiy.gmail.com>
2021-08-16 13:43:04 +0300

    4. final [#334073 #190487]00

---
+++ src/main/java/ru/job4j/ooa/Airbus.java
@@ -0,0 +1,39 @@
+package ru.job4j.ooa;
+
+public final class Airbus extends Aircraft {
+    private static final int COUNT_ENGINE = 2;
+
+    private String name;
+
+    public Airbus(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public void printModel() {
+        System.out.println("Модель самолета: " + name);
+    }
+
+    public void printCountEngine() {
+        if ("A320".equals(this.name)) {
+            System.out.println("Количество двигателей равно: " + 4);
+        } else {
+            System.out.println("Количество двигателей равно: " + COUNT_ENGINE);
+        }
+    }
+
+    @Override
+    public String toString() {
+        return "Airbus{"
+                + "name='" + name + '\''
+                + '}';
+    }
+}
