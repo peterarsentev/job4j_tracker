278a2e4ba9b44b02ea130cb18693fbcf7a89979c
chydikO_x220 <oleg.chudnovskiy.gmail.com>
2021-08-26 16:41:40 +0300

    4. final [#334073 #190487]00 refactoring

--- src/main/java/ru/job4j/ooa/Airbus.java
+++ src/main/java/ru/job4j/ooa/Airbus.java
@@ -23,11 +23,8 @@ public final class Airbus extends Aircraft {
     }
 
     public void printCountEngine() {
-        if ("A320".equals(this.name)) {
-            System.out.println("Количество двигателей равно: " + 4);
-        } else {
-            System.out.println("Количество двигателей равно: " + COUNT_ENGINE);
-        }
+        int count = "A380".equals(name) ? 4 : COUNT_ENGINE;
+        System.out.println("Количество двигателей равно: " + count);
     }
 
     @Override
