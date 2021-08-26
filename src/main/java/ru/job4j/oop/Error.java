05ccc4e057b23871de70d1a0c8e01ab0b00abbd0
Veronika Lapenok <veronika.lapenok@mail.ru>
2021-08-26 14:56:37 +0300

    1.7. Конструктор [#173889]

---
+++ src/main/java/ru/job4j/oop/Error.java
@@ -0,0 +1,31 @@
+package ru.job4j.oop;
+
+public class Error {
+    private boolean active;
+    private int status;
+    private String message;
+
+    public Error() {
+    }
+
+    public Error(boolean active, int status, String message) {
+        this.active = active;
+        this.status = status;
+        this.message = message;
+    }
+
+    public void printInfo() {
+        System.out.println("Active: " + active);
+        System.out.println("Status: " + status);
+        System.out.println("Message: " + message);
+    }
+
+    public static void main(String[] args) {
+        Error error = new Error();
+        Error error404 = new Error(false, 404, "Not found.");
+        Error error503 = new Error(true, 503, "Service unavailable");
+        error.printInfo();
+        error404.printInfo();
+        error503.printInfo();
+    }
+}
