ed7827813b4954c3e7e64c8477b10ed4a21fec5b
Veronika Lapenok <veronika.lapenok@mail.ru>
2021-08-31 10:32:08 +0300

    1.8. Состояние объекта [#167388 #195985]

--- src/main/java/ru/job4j/oop/Battery.java
+++ src/main/java/ru/job4j/oop/Battery.java
@@ -8,12 +8,12 @@ public class Battery {
     }
 
     public void exchange(Battery another) {
-        this.load = this.load - another.load;
-        another.load = 0;
+        another.load = this.load + another.load;
+        this.load = 0;
     }
 
     public static void main(String[] args) {
-        Battery first = new Battery(90);
+        Battery first = new Battery(70);
         Battery second = new Battery(30);
         first.exchange(second);
         System.out.println("first: " + first.load + ". second: " + second.load);
