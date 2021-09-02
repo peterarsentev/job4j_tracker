04f8c1dabf2cb82cdaa71497d3e50f08a2d15ed9
Veronika Lapenok <veronika.lapenok@mail.ru>
2021-09-02 11:27:49 +0300

    2.2. Рефакторинг - Площадь треугольника. [#122642 #197840]

--- src/main/java/ru/job4j/oop/Triangle.java
+++ src/main/java/ru/job4j/oop/Triangle.java
@@ -19,7 +19,7 @@ public class Triangle {
 
     public boolean exist(double ab, double ac, double bc) {
         boolean result = false;
-        if (ab + ac > bc || ab + bc > ac || bc + ac > ab) {
+        if (ab + ac > bc && ab + bc > ac && bc + ac > ab) {
             result = true;
         }
         return result;
--- src/test/java/ru/job4j/oop/TriangleTest.java
+++ src/test/java/ru/job4j/oop/TriangleTest.java
@@ -3,16 +3,28 @@ package ru.job4j.oop;
 import static org.hamcrest.Matchers.closeTo;
 import static org.junit.Assert.assertThat;
 
+import org.junit.Assert;
 import org.junit.Test;
 
 public class TriangleTest {
     @Test
-    public void area() {
+    public void ifTriangleExist() {
         Point a = new Point(0, 0);
         Point b = new Point(4, 0);
         Point c = new Point(0, 4);
         Triangle triangle = new Triangle(a, b, c);
         double result = triangle.area();
         assertThat(result, closeTo(8, 0.001));
+    }
+
+    @Test
+    public void ifTriangleNotExist() {
+        Point a = new Point(0, 0);
+        Point b = new Point(0, 0);
+        Point c = new Point(3, 9);
+        Triangle triangle = new Triangle(a, b, c);
+        double result = triangle.area();
+        double expected = -1;
+        Assert.assertEquals(expected, result, 0.01);
     }
 }
