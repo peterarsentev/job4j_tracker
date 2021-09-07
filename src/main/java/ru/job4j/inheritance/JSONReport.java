72e8e5826f0916be418a51a2a72fd5977d37c0a3
Veronika Lapenok <veronika.lapenok@mail.ru>
2021-09-07 15:27:53 +0300

    4. Переопределение [#174890 #198476]

--- src/main/java/ru/job4j/inheritance/JSONReport.java
+++ src/main/java/ru/job4j/inheritance/JSONReport.java
@@ -3,8 +3,16 @@ package ru.job4j.inheritance;
 public class JSONReport extends TextReport {
     @Override
     public String generate(String name, String body) {
-        return '"' + name + '"' + " : " + '"' + name + '"' +
-                System.lineSeparator() +
-                '"' + body + '"' + " : " + '"' + body + '"';
+        return  "{" + System.lineSeparator() + "  " +
+                '"' + name + '"' + " : " + '"' + name + '"' + "," +
+                System.lineSeparator() + "  " +
+                '"' + body + '"' + " : " + '"' + body + '"' +
+                System.lineSeparator() + "}";
+    }
+
+    public static void main(String[] args) {
+        JSONReport report = new JSONReport();
+        String text = report.generate("name", "body");
+        System.out.println(text);
     }
 }
