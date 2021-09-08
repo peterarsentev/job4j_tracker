0de09a46e01407cd0ffa7ef425df18fdd7954696
Veronika Lapenok <veronika.lapenok@mail.ru>
2021-09-08 15:49:27 +0300

    3. Массивы и модели. [#174892 #200462]

--- src/main/java/ru/job4j/pojo/Library.java
+++ src/main/java/ru/job4j/pojo/Library.java
@@ -33,7 +33,7 @@ public class Library {
 
         for (int index = 0; index < books.length; index++) {
             Book book = books[index];
-            if (book.getName().equals("Clean code")) {
+            if ("Clean code".equals(book.getName())) {
                 System.out.println(book.getName() + " - " + book.getNumOfPages());
             }
         }
