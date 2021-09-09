437cb55735ea4e082a64f561094d6e6017ff0634
Veronika Lapenok <veronika.lapenok@mail.ru>
2021-09-09 10:10:12 +0300

    3.2. Удаление моделей из массива. [#310016 #200587]

--- src/main/java/ru/job4j/pojo/ShopDrop.java
+++ src/main/java/ru/job4j/pojo/ShopDrop.java
@@ -5,8 +5,8 @@ public class ShopDrop {
         products[index] = null;
         for (int i = index; i < products.length - 1; i++) {
             products[i] = products[i + 1];
-            products[i + 1] = null;
         }
+        products[products.length - 1] = null;
         return products;
     }
 }
