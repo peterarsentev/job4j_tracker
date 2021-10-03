6a09ebf397869fd4eed44ddcccc638326d9bf0bf
Zver <krutaxe@mail.ru>
2021-10-03 12:31:33 +0300

    Local variable 'result' is redundant

---
+++ src/main/java/ru/job4j/oop/Triangle.java
@@ -0,0 +1,34 @@
+package ru.job4j.oop;
+
+public class Triangle {
+    private Point first;
+    private Point second;
+    private Point third;
+
+    public  Triangle(Point ap, Point bp, Point cp) {
+        this.first = ap;
+        this.second = bp;
+        this.third = cp;
+    }
+
+    public double semiPerimeter(double a, double b, double c) {
+        return (a + b + c) / 2;
+    }
+
+    public  boolean exist(double ab, double ac, double bc) {
+        boolean result = ab + ac > bc && ac + bc > ab && ab + bc > ac;
+        return result;
+    }
+
+    public double area() {
+        double rsl = -1;
+        double ab = first.distance(second);
+        double ac = first.distance(third);
+        double bc = second.distance(third);
+        if (this.exist(ab, ac, bc)) {
+            double p = semiPerimeter(ab, ac, bc);
+            rsl = Math.sqrt(p * (p - ab) * (p - ac) * (p - bc));;
+        }
+        return rsl;
+    }
+}
---
+++ src/test/java/ru/job4j/oop/TriangleTest.java
@@ -0,0 +1,19 @@
+package ru.job4j.oop;
+
+import static org.hamcrest.Matchers.closeTo;
+import static org.junit.Assert.assertThat;
+
+import org.junit.Test;
+
+public class TriangleTest {
+
+    @Test
+    public void area() {
+        Point a = new Point(0, 0);
+        Point b = new Point(4, 0);
+        Point c = new Point(0, 4);
+        Triangle triangle = new Triangle(a, b, c);
+        double rsl = triangle.area();
+        assertThat(rsl, closeTo(8, 0.001));
+    }
+}
