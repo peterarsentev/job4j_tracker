5a757def30001ba33c821c8727cd00394a0f51cc
Veronika Lapenok <veronika.lapenok@mail.ru>
2021-10-05 18:53:06 +0300

    0.2. Кидаем исключение - throw new RuntimeException [#219365]

---
+++ src/main/java/ru/job4j/ex/Fact.java
@@ -0,0 +1,18 @@
+package ru.job4j.ex;
+
+public class Fact {
+    public static void main(String[] args) {
+        new Fact().calc(-1);
+    }
+
+    public int calc(int n) {
+        if (n < 0) {
+            throw new IllegalArgumentException("n should be more or equal 0");
+        }
+        int rsl = 1;
+        for (int index = 0; index <= n; index++) {
+            rsl *= index;
+        }
+        return rsl;
+    }
+}
