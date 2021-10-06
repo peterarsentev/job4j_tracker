2546dd49cd5cfa1440eaa989c8276a61db6da552
maria <kotovalovam@yandex.ru>
2021-10-06 23:53:07 +0700

    Конструктор [#173889]

---
+++ src/main/java/ru/job4j/oop/Error.java
@@ -0,0 +1,29 @@
+package ru.job4j.oop;
+
+public class Error {
+    boolean active;
+    int status;
+    String message;
+
+    public Error () {
+    }
+
+    public Error (boolean active, int status, String message) {
+        this.active = active;
+        this.status = status;
+        this.message = message;
+    }
+
+    public void printInfo () {
+        System.out.println("active: " + this.active + " status: " + this.status + " message: " + this.message);
+    }
+
+    public static void main(String[] args) {
+        Error error1 = new Error(true, 1, "ОК");
+        Error error2 = new Error(false, 2, "not ОК");
+        Error error3 = new Error();
+        error1.printInfo();
+        error2.printInfo();
+        error3.printInfo();
+    }
+}
