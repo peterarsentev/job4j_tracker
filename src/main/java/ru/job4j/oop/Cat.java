3db1e17a4eeeee01445ab9aae1d635a246a540f5
maria <kotovalovam@yandex.ru>
2021-10-06 22:55:53 +0700

    Поля объекта [#166083]

---
+++ src/main/java/ru/job4j/oop/Cat.java
@@ -0,0 +1,32 @@
+package ru.job4j.oop;
+
+public class Cat {
+
+    private String food;
+    private String name;
+
+    public void show() {
+        System.out.println("name: " + this.name + " food: " + this.food);
+    }
+
+    public void eat(String meat) {
+        this.food = meat;
+    }
+
+    public void giveNick(String nick) {
+        this.name = nick;
+    }
+
+    public static void main(String[] args) {
+        System.out.println("There are gav's food.");
+        Cat gav = new Cat();
+        gav.eat("kotleta");
+        gav.giveNick("gav");
+        gav.show();
+        System.out.println("There are black's food.");
+        Cat black = new Cat();
+        black.eat("fish");
+        black.giveNick("black");
+        black.show();
+    }
+}
