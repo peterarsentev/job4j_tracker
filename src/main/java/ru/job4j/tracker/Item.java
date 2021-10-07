a0b98925f91f811fc17c1b9d29e34cf9e92df35c
Zver <krutaxe@mail.ru>
2021-10-07 13:00:16 +0300

    2. Модель данных. [#174683]

--- src/main/java/ru/job4j/tracker/Item.java
+++ src/main/java/ru/job4j/tracker/Item.java
@@ -3,20 +3,24 @@ package ru.job4j.tracker;
 import java.time.LocalDateTime;
 
 public class Item {
-    LocalDateTime created = LocalDateTime.now();
+    private LocalDateTime created = LocalDateTime.now();
+    private int id;
+    private String name;
+    public Item(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+    public void setId(int id) {
+        this.id = id;
+    }
+    public Item(String name) {
+        this.name = name;
+    }
+    public  Item() {
+    }
 
     public LocalDateTime getCreated() {
         return created;
-    }
-
-    private int id;
-
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
     }
 
     public String getName() {
@@ -27,17 +31,8 @@ public class Item {
         this.name = name;
     }
 
-    private String name;
-
-    public  Item() {
+    public int getId() {
+        return id;
     }
 
-    public Item(String name) {
-        this.name = name;
-    }
-
-    public Item(int id, String name) {
-        this.id = id;
-        this.name = name;
-    }
 }
