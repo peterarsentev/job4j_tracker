9b5d47bfd5083f1fc4dbe624f560c3a2c16ede84
maria <kotovalovam@yandex.ru>
2021-10-07 19:12:28 +0700

    Состояние объекта [#167388]

---
+++ src/main/java/ru/job4j/oop/Battery.java
@@ -0,0 +1,22 @@
+package ru.job4j.oop;
+
+public class Battery {
+    private int load;
+
+    public Battery (int load) {
+        this.load = load;
+    }
+
+    public void exchange(Battery another) {
+        another.load = another.load + this.load;
+        this.load = 0;
+    }
+
+    public static void main(String[] args) {
+        Battery first = new Battery(10);
+        Battery second = new Battery(1);
+        System.out.println("first : " + first.load + ". second : " + second.load);
+        first.exchange(second);
+        System.out.println("first : " + first.load + ". second : " + second.load);
+    }
+}
