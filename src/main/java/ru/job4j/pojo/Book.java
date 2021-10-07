cfb9d93be85363a3ffff2d8f542e41f4a0d4e1fd
Zver <krutaxe@mail.ru>
2021-10-07 22:37:56 +0300

    3. Массивы и модели. [#174892]

---
+++ src/main/java/ru/job4j/pojo/Book.java
@@ -0,0 +1,42 @@
+package ru.job4j.pojo;
+
+import java.util.Objects;
+
+public class Book {
+    private String name;
+    private int pages;
+
+    public Book(String name, int pages) {
+        this.name = name;
+        this.pages = pages;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Book book = (Book) o;
+        return pages == book.pages && Objects.equals(name, book.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, pages);
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getPages() {
+        return pages;
+    }
+
+    public void setPages(int pages) {
+        this.pages = pages;
+    }
+}
---
+++ src/main/java/ru/job4j/pojo/Library.java
@@ -0,0 +1,42 @@
+package ru.job4j.pojo;
+
+import com.sun.source.tree.IfTree;
+
+public class Library {
+    public static void main(String[] args) {
+        Book red = new Book("Red", 98);
+        Book blue = new Book("Blue", 150);
+        Book green = new Book("Green", 44);
+        Book cleanCode = new Book("Clean code", 100);
+
+        Book[] books = new Book[4];
+
+        books[0] = red;
+        books[1] = cleanCode;
+        books[2] = green;
+        books[3] = blue;
+
+        for (int i = 0; i < books.length; i++) {
+            Book bk = books[i];
+            System.out.println(bk.getName() + " - " + bk.getPages());
+        }
+
+        System.out.println("Переставим местами книги с индексом 0 и 3.");
+        for (int i = 0; i < books.length; i++) {
+            Book bk = books[i];
+            if (i == 0) {
+                bk = books[3];
+            } else if (i == 3) {
+                bk = books[0];
+            }
+            System.out.println(bk.getName() + " - " + bk.getPages());
+        }
+
+        System.out.println("Цикл с выводом книг с именем \"Clean code\"");
+        for (int i = 0; i < books.length; i++) {
+            Book bk = books[i];
+            if (bk.getName().equals("Clean code"))
+            System.out.println(bk.getName());
+        }
+    }
+}
