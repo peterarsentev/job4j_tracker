7ca293c0912ee6583863bc540e5c80a1f6cc8d70
Zver <krutaxe@mail.ru>
2021-10-07 15:57:51 +0300

    6. Date. Отображение даты. [#310017 #213021]

--- src/main/java/ru/job4j/tracker/Item.java
+++ src/main/java/ru/job4j/tracker/Item.java
@@ -6,16 +6,20 @@ public class Item {
     private LocalDateTime created = LocalDateTime.now();
     private int id;
     private String name;
+
     public Item(int id, String name) {
         this.id = id;
         this.name = name;
     }
+
     public void setId(int id) {
         this.id = id;
     }
+
     public Item(String name) {
         this.name = name;
     }
+
     public  Item() {
     }
 
--- src/main/java/ru/job4j/tracker/StartUI.java
+++ src/main/java/ru/job4j/tracker/StartUI.java
@@ -5,9 +5,9 @@ import java.time.format.DateTimeFormatter;
 public class StartUI {
 
     public static void main(String[] args) {
-        Item Item = new Item();
+        Item item = new Item();
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MMMM-EEEE-yyyy HH:mm:ss");
-        String createdFormat = Item.getCreated().format(formatter);
+        String createdFormat = item.getCreated().format(formatter);
         System.out.println("Текущие дата и время после форматирования: " + createdFormat);
     }
 }
