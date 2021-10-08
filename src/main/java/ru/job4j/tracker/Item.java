82b968b3b494810122d244a14ae25903d7b28929
Zver <krutaxe@mail.ru>
2021-10-09 00:59:09 +0300

    5. Tracker - хранилище [#396 #213919]

--- src/main/java/ru/job4j/tracker/Item.java
+++ src/main/java/ru/job4j/tracker/Item.java
@@ -1,6 +1,7 @@
 package ru.job4j.tracker;
 
 import java.time.LocalDateTime;
+import java.util.Objects;
 
 public class Item {
     private LocalDateTime created = LocalDateTime.now();
@@ -10,6 +11,20 @@ public class Item {
     public Item(int id, String name) {
         this.id = id;
         this.name = name;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Item item = (Item) o;
+        return id == item.id && Objects.equals(created, item.created)
+                && Objects.equals(name, item.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(created, id, name);
     }
 
     public void setId(int id) {
--- src/main/java/ru/job4j/tracker/Tracker.java
+++ src/main/java/ru/job4j/tracker/Tracker.java
@@ -1,5 +1,7 @@
 package ru.job4j.tracker;
 
+import java.util.Arrays;
+
 public class Tracker {
     private final Item[] items = new Item[100];
     private int ids = 1;
@@ -9,6 +11,36 @@ public class Tracker {
         item.setId(ids++);
         items[size++] = item;
         return item;
+    }
+
+    public Item[] findAll() {
+        Item[] nameWithoutNull = new Item[items.length];
+        int size = 0;
+        for (int index = 0; index < items.length; index++) {
+            Item item = items[index];
+            if (item != null) {
+                nameWithoutNull[size] = item;
+                size++;
+            }
+        }
+        nameWithoutNull = Arrays.copyOf(nameWithoutNull, size);
+        for (int index = 0; index < nameWithoutNull.length; index++) {
+        }
+        return nameWithoutNull;
+    }
+
+    public Item[] findByName(String key) {
+        Item[] name = new Item[size];
+        int id = 0;
+        for (int index = 0; index < size; index++) {
+            Item item = items[index];
+            if (item.getName().equals(key)) {
+                name[id] = item;
+                id++;
+            }
+        }
+        return Arrays.copyOf(name, id);
+
     }
 
     public Item findById(int id) {
