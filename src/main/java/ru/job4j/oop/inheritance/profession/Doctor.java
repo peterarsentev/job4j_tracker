ff8f5d8d4048f2c370e975146fa5dc33655731cf
maria <kotovalovam@yandex.ru>
2021-10-10 20:19:08 +0700

    Реализация профессий в коде [#6837 #214384]

--- src/main/java/ru/job4j/oop/inheritance/profession/Doctor.java
+++ src/main/java/ru/job4j/oop/inheritance/profession/Doctor.java
@@ -1,4 +1,4 @@
-package ru.job4j.oop.profession;
+package ru.job4j.oop.inheritance.profession;
 
 public class Doctor extends Profession{
     private int experience;
--- src/main/java/ru/job4j/oop/inheritance/profession/Profession.java
+++ src/main/java/ru/job4j/oop/inheritance/profession/Profession.java
@@ -1,4 +1,4 @@
-package ru.job4j.oop.profession;
+package ru.job4j.oop.inheritance.profession;
 
 public class Profession {
     private String name;
