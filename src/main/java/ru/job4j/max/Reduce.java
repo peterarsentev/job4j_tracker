b726c0be29250f81cd76f971b56b6867a19ffd8d
maria <kotovalovam@yandex.ru>
2021-10-10 18:49:59 +0700

    Зона видимости переменных [#180147]

---
+++ src/main/java/ru/job4j/max/Reduce.java
@@ -0,0 +1,22 @@
+package ru.job4j.max;
+
+public class Reduce {
+    private int[] array;
+
+    public void to(int[] array) {
+        this.array = array;
+    }
+
+    public void print() {
+        for (int index = 0; index < array.length; index++) {
+            System.out.println(array[index]);
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] array = {1, 2, 3};
+        Reduce reduce = new Reduce();
+        reduce.to(array);
+        reduce.print();
+    }
+}
