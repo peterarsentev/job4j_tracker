c09809aef175a5d4902e43a278f9615dc3b1b27f
maria <kotovalovam@yandex.ru>
2021-10-11 18:52:24 +0700

    Переопределение [#174890 #214400]

--- src/main/java/ru/job4j/oop/inheritance/JSONReport.java
+++ src/main/java/ru/job4j/oop/inheritance/JSONReport.java
@@ -5,7 +5,6 @@ public class JSONReport extends TextReport{
 
     @Override
     public String generate(String name, String body) {
-        char dm = (char) 34;
         return "{" + System.lineSeparator() +
                 "  \"name\" : " + "\"" + name + "\"," + System.lineSeparator() +
                 "  \"body\" : " + "\"" + body + "\"" + System.lineSeparator() +
