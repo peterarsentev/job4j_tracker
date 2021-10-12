361f7c3354db2f57521dca542532b47755d3fccb
Zver <krutaxe@mail.ru>
2021-10-12 21:30:25 +0300

    1. Чтение из консоли. Класс Scanner. [#179514 #214450]

--- src/main/java/ru/job4j/io/MagicBall.java
+++ src/main/java/ru/job4j/io/MagicBall.java
@@ -13,6 +13,8 @@ public class MagicBall {
             System.out.println("Да!");
         } else if (answer == 1) {
             System.out.println("Нет.");
-        } else System.out.println("Может быть.");
+        } else {
+            System.out.println("Может быть.");
+        }
     }
 }
