80b3a972296ddd1baea61cfecfb7f83aa9022e5b
Veronika Lapenok <veronika.lapenok@mail.ru>
2021-10-13 12:00:00 +0300

    1. Обеспечить бесперебойную работу приложения Tracker [#789]

---
+++ src/main/java/ru/job4j/tracker/ValidateInput.java
@@ -0,0 +1,18 @@
+package ru.job4j.tracker;
+
+public class ValidateInput extends ConsoleInput {
+    @Override
+    public int askInt(String question) {
+        boolean invalid = true;
+        int value = -1;
+        do {
+            try {
+                value = super.askInt(question);
+                invalid = false;
+            } catch (NumberFormatException nfe) {
+                System.out.println("Please enter validate data again.");
+            }
+        } while (invalid);
+        return value;
+    }
+}
