40c2b74d2d8c1377bb39e2e6bb13402f82ed3640
Zver <krutaxe@mail.ru>
2021-10-15 20:29:12 +0300

    0.3. Тестирование исключений с junit [#219366]

---
+++ src/test/java/ru/job4j/ex/FactTest.java
@@ -0,0 +1,13 @@
+package ru.job4j.ex;
+
+import org.junit.Test;
+
+
+public class FactTest {
+
+    @Test(expected = IllegalArgumentException.class)
+    public void findFactorial  () {
+        new Fact().calc(-2);
+
+    }
+}
