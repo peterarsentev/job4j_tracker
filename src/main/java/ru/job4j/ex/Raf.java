6fc53d0f65c5eb250de36719b281a630feeb25b1
Zver <krutaxe@mail.ru>
2021-10-17 20:59:12 +0300

    0. Что такое исключение. [#3821 #216545]

---
+++ src/main/java/ru/job4j/ex/Raf.java
@@ -0,0 +1,15 @@
+package ru.job4j.ex;
+
+public class Raf{
+    int a;
+    String name;
+
+    public Raf(int a, String name) {
+        this.a = a;
+        this.name = name;
+    }
+
+    public static void main(String[] args) {
+        System.out.println();
+    }
+}
