0b31c56a33ee7e263be15ebb69eb90a65a4c552b
Veronika Lapenok <veronika.lapenok@mail.ru>
2021-10-22 11:08:06 +0300

    0.6. Error - исключения, связанные с работой виртуальной машины [#219370 #215445]

--- src/main/java/ru/job4j/ex/FactRec.java
+++ src/main/java/ru/job4j/ex/FactRec.java
@@ -2,13 +2,10 @@ package ru.job4j.ex;
 
 public class FactRec {
     public static int calc(int n) {
-        int result;
         if (n == 0 || n == 1) {
-            result = 1;
-        } else {
-            result = calc(n - 1) * n;
+            return 1;
         }
-        return result;
+        return calc(n - 1) * n;
     }
 
     public static void main(String[] args) {
