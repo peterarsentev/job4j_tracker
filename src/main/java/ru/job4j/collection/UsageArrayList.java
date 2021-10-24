1aa0a25b35ad53ad932f65426405c0fc7db5170c
Zver <krutaxe@mail.ru>
2021-10-24 15:22:12 +0300

    0. Коллекции, ArrayList, List, Обобщения [#10094]

---
+++ src/main/java/ru/job4j/collection/UsageArrayList.java
@@ -0,0 +1,15 @@
+package ru.job4j.collection;
+
+import java.util.ArrayList;
+
+public class UsageArrayList {
+    public static void main(String[] args) {
+        ArrayList<String> names = new ArrayList<String>();
+        names.add("Petr");
+        names.add("Ivan");
+        names.add("Stepan");
+        for (Object value : names) {
+            System.out.println(value);
+        }
+    }
+}
