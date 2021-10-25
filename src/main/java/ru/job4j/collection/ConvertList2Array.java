640c41eb648598dc8348ab31a7d84ae1f4f8a628
Zver <krutaxe@mail.ru>
2021-10-25 22:36:47 +0300

    3. Конвертация ArrayList в двухмерный массив [#10035]

---
+++ src/main/java/ru/job4j/collection/ConvertList2Array.java
@@ -0,0 +1,30 @@
+package ru.job4j.collection;
+
+import java.util.List;
+
+public class ConvertList2Array {
+    public static int[][] toArray(List<Integer> list, int cells) {
+        int groups = (int) Math.ceil((double) list.size() / cells);
+        int[][] array = new int[groups][cells];
+        int row = 0, cell = 0;
+        for (Integer num : list) {
+            array[row][cell] = num;
+            if (++cell >= cells) {
+                row++;
+                cell = 0;
+            }
+        }
+        return array;
+    }
+
+    public static void main(String[] args) {
+        List<Integer> list = List.of(9, 8, 7, 6, 5, 4, 3, 2);
+        int[][] rsl = toArray(list, 3);
+        for (int[] row : rsl) {
+            for (int cell : row) {
+                System.out.print(cell + " ");
+            }
+            System.out.println();
+        }
+    }
+}
---
+++ src/test/java/ru/job4j/collection/ConvertList2ArrayTest.java
@@ -0,0 +1,26 @@
+package ru.job4j.collection;
+
+import org.junit.Ignore;
+import org.junit.Test;
+
+import java.util.Arrays;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+@Ignore
+public class ConvertList2ArrayTest {
+    @Test
+    public void when7ElementsThen9() {
+        int[][] result = ConvertList2Array.toArray(
+                Arrays.asList(9, 8, 7, 6, 5, 4, 3),
+                3
+        );
+        int[][] expect = {
+                {9, 8, 7},
+                {6, 5, 4},
+                {3, 0, 0}
+        };
+        assertThat(result, is(expect));
+    }
+}
