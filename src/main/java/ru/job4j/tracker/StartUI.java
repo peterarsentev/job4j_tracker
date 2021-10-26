2af85e4c46fbcf98a49864c635bfca376d00a7e5
maria <kotovalovam@yandex.ru>
2021-10-26 13:46:44 +0700

    Реализация класса StartUI. Добавление заявки [#500744]

--- src/main/java/ru/job4j/tracker/StartUI.java
+++ src/main/java/ru/job4j/tracker/StartUI.java
@@ -19,10 +19,17 @@ public class StartUI {
             showMenu();
             System.out.print("Select: ");
             int select = Integer.parseInt(scanner.nextLine());
-            if (select != 6) {
-                System.out.println("Пользователь выбрал: " + select);
-            } else {
+            if (select == 0) {
+                System.out.println("=== Create a new Item ===");
+                System.out.print("Enter name: ");
+                String name = scanner.nextLine();
+                Item item = new Item(name);
+                tracker.add(item);
+                System.out.println("Добавленная заявка: " + item);
+            } else if (select == 6) {
                 run = false;
+            } else {
+                System.out.println("Пользователь выбрал: " + select);
             }
         }
 
