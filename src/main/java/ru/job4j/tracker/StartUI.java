490c628b3ae71ba589f7b504eec270f8d3f4db24
maria <kotovalovam@yandex.ru>
2021-10-26 14:15:15 +0700

    Реализация класса StartUI. Изменение заявки [#500746]

--- src/main/java/ru/job4j/tracker/StartUI.java
+++ src/main/java/ru/job4j/tracker/StartUI.java
@@ -29,12 +29,24 @@ public class StartUI {
             } else if (select == 1) {
                 System.out.println("=== Show all items ===");
                 Item[] items = tracker.findAll();
-                if (items.length > 0 ) {
+                if (items.length > 0) {
                     for (Item item : items) {
                         System.out.println(item);
                     }
                 } else {
                     System.out.println("Хранилище еще не содержит заявок");
+                }
+            } else if (select == 2) {
+                System.out.println("=== Edit item ===");
+                System.out.print("Enter id: ");
+                int id = Integer.parseInt(scanner.nextLine());
+                System.out.print("Enter name: ");
+                String name = scanner.nextLine();
+                Item item = new Item(name);
+                if (tracker.replace(id, item)) {
+                    System.out.println("Заявка изменена успешно.");
+                } else {
+                    System.out.println("Ошибка замены заявки.");
                 }
             } else if (select == 6) {
                 run = false;
