20e4620a4a4c756682b15a3c2277c23f2a59d412
Zver <krutaxe@mail.ru>
2021-10-28 23:34:00 +0300

    1. Уникальные задачи. [#212653]

---
+++ src/main/java/ru/job4j/collection/FullSearch.java
@@ -0,0 +1,14 @@
+package ru.job4j.collection;
+
+import java.util.HashSet;
+import java.util.List;
+
+public class FullSearch {
+    public static HashSet<String> extractNumber(List<Task> list) {
+        HashSet<String> numbers = new HashSet<>();
+        for (Task t : list) {
+            numbers.add(t.getNumber());
+        }
+        return numbers;
+    }
+}
