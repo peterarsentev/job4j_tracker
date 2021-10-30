6fdf68a5a1a775b49a696e11f3c544d3df881e6f
Zver <krutaxe@mail.ru>
2021-10-30 11:51:25 +0300

    2. Идентичные тексты [#212655 #222313]

--- src/main/java/ru/job4j/collection/UniqueText.java
+++ src/main/java/ru/job4j/collection/UniqueText.java
@@ -5,7 +5,7 @@ import java.util.HashSet;
 
 public class UniqueText {
     public static boolean isEquals(String originText, String duplicateText) {
-        boolean rsl = false;
+        boolean rsl = true;
         String[] origin = originText.split(" ");
         String[] text = duplicateText.split(" ");
         HashSet<String> check = new HashSet<>();
@@ -13,9 +13,7 @@ public class UniqueText {
             check.add(i);
         }
         for (String j : text) {
-            if (check.contains(j)) {
-                rsl = true;
-            } else {
+            if (!check.contains(j)) {
                 rsl = false;
                 break;
             }
