3d115586baab45931fefc53104cc6132299c38eb
Zver <krutaxe@mail.ru>
2021-10-31 17:05:00 +0300

    1. Паспорт и жители [#212656 #222417]

--- src/main/java/ru/job4j/collection/PassportOffice.java
+++ src/main/java/ru/job4j/collection/PassportOffice.java
@@ -8,16 +8,30 @@ public class PassportOffice {
     private Map<String, Citizen> citizens = new HashMap<>();
 
     public boolean add(Citizen citizen) {
-        boolean rsl = true;
-        if (!citizens.containsKey(citizen)) {
+        boolean rsl = false;
+        if (!citizens.containsKey(citizen.getPassport())) {
             citizens.put(citizen.getPassport(), citizen);
-        } else {
-            rsl = false;
+            rsl = true;
         }
         return rsl;
     }
 
     public Citizen get(String passport) {
         return citizens.get(passport);
+    }
+
+    public static void main(String[] args) {
+        Citizen citizen = new Citizen("2f44a", "Petr Arsentev");
+        Citizen citizen1 = new Citizen("2f44a", "Petr Arsentev");
+        Citizen citizen2 = new Citizen("2f44", "Petr Arsentev");
+        Citizen citizen3 = new Citizen("2f44", "Petr Arsentev");
+
+        PassportOffice office = new PassportOffice();
+        System.out.println(office.add(citizen));
+        System.out.println(office.add(citizen1));
+        System.out.println(office.add(citizen));
+        System.out.println(office.add(citizen2));
+        System.out.println(office.add(citizen3));
+
     }
 }
--- src/test/java/ru/job4j/collection/PassportOfficeTest.java
+++ src/test/java/ru/job4j/collection/PassportOfficeTest.java
@@ -14,4 +14,14 @@ public class PassportOfficeTest {
         office.add(citizen);
         assertThat(office.get(citizen.getPassport()), is(citizen));
     }
+
+    @Test
+    public void addDuplicate() {
+        Citizen citizen = new Citizen("2f44a", "Petr Arsentev");
+        Citizen citizenTwo = new Citizen("2f44a", "Petr Arsentev");
+        PassportOffice office = new PassportOffice();
+        office.add(citizen);
+        assertFalse(office.add(citizenTwo));
+
+    }
 }
