04571840dd91eedc2d2ac3b9dc6e9aa8944a3a90
parsentev <parsentev@yandex.ru>
2021-11-11 11:51:37 +0300

    upgrade junit and checkstyle rules

--- src/test/java/ru/job4j/tracker/TrackerTest.java
+++ src/test/java/ru/job4j/tracker/TrackerTest.java
@@ -1,8 +1,9 @@
 package ru.job4j.tracker;
 
 import org.junit.Test;
+
+import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.is;
-import static org.junit.Assert.assertThat;
 
 public class TrackerTest {
     @Test
