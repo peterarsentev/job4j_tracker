f8d6b94ec3c90b0355be2e8bdceb4a3cdc850d77
Zver <krutaxe@mail.ru>
2021-11-15 12:52:31 +0300

    3. Уникальность элементов и сортировка. [#110225]

--- src/main/java/ru/job4j/stream/Pro.java
+++ src/main/java/ru/job4j/stream/Pro.java
@@ -1,12 +1,13 @@
 package ru.job4j.stream;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
 public class Pro {
     public List<Address> collect(List<Profile> profiles) {
         return profiles.stream().map(Profile::getAddress)
+                .sorted((x, y) -> x.getCity().compareTo(y.getCity()))
+                .distinct()
                 .collect(Collectors.toList());
     }
 }
--- src/test/java/ru/job4j/stream/ProTest.java
+++ src/test/java/ru/job4j/stream/ProTest.java
@@ -8,21 +8,25 @@ import static org.hamcrest.MatcherAssert.assertThat;
 
 public class ProTest {
     @Test
-    public void whenCollectAddressAndFilterHome() {
-        Address address1 = new Address("KZN", "Mira", 20, 235);
+    public void whenCollectAddressSortCity() {
+        Address address1 = new Address("KZN", "Lenina", 7, 6);
         Address address2 = new Address("MSK", "Gagarina", 3, 2);
         Address address3 = new Address("KZN", "Lenina", 7, 6);
+        Address address4 = new Address("MSK", "Gagarina", 3, 2);
+        Address address5 = new Address("AMV", "Mendeleeva", 7, 1);
 
         Profile profile1 = new Profile(address1);
         Profile profile2 = new Profile(address2);
         Profile profile3 = new Profile(address3);
+        Profile profile4 = new Profile(address4);
+        Profile profile5 = new Profile(address5);
 
-        List<Profile> profiles = List.of(profile1, profile2, profile3);
+        List<Profile> profiles = List.of(profile1, profile2, profile3, profile4, profile5);
+        List<Address> expected = List.of(address5, address1, address2);
         Pro pro = new Pro();
         List<Address> rsl = pro.collect(profiles);
-        List<Address> expected = List.of(address1, address2, address3);
-
         assertThat(rsl, is(expected));
+
     }
 
 }
