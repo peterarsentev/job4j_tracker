9db27d33dea518051baebe16363f50f7ef6fdc75
Zver <krutaxe@mail.ru>
2021-11-15 12:18:43 +0300

    2. Список адресов [#110062]

--- src/main/java/ru/job4j/stream/Pro.java
+++ src/main/java/ru/job4j/stream/Pro.java
@@ -7,23 +7,6 @@ import java.util.stream.Collectors;
 public class Pro {
     public List<Address> collect(List<Profile> profiles) {
         return profiles.stream().map(Profile::getAddress)
-                .filter(e -> e.getHome() > 10)
                 .collect(Collectors.toList());
-    }
-
-    public static void main(String[] args) {
-        Address address1 = new Address("KZN", "STR", 2, 4);
-        Address address2 = new Address("MSK", "SOR", 12, 1);
-        Address address3 = new Address("ORN", "SIR", 3, 8);
-
-        Profile profile1 = new Profile(address1);
-        Profile profile2 = new Profile(address2);
-        Profile profile3 = new Profile(address3);
-
-        List<Profile> profiles = List.of(profile1, profile2, profile3);
-
-        Pro pro = new Pro();
-        List<Address> list = pro.collect(profiles);
-        System.out.println(list);
     }
 }
--- src/test/java/ru/job4j/stream/ProTest.java
+++ src/test/java/ru/job4j/stream/ProTest.java
@@ -11,7 +11,7 @@ public class ProTest {
     public void whenCollectAddressAndFilterHome() {
         Address address1 = new Address("KZN", "Mira", 20, 235);
         Address address2 = new Address("MSK", "Gagarina", 3, 2);
-        Address address3 = new Address("ORN", "Lenina", 7, 6);
+        Address address3 = new Address("KZN", "Lenina", 7, 6);
 
         Profile profile1 = new Profile(address1);
         Profile profile2 = new Profile(address2);
@@ -20,26 +20,9 @@ public class ProTest {
         List<Profile> profiles = List.of(profile1, profile2, profile3);
         Pro pro = new Pro();
         List<Address> rsl = pro.collect(profiles);
-        List<Address> expected = List.of(address1);
+        List<Address> expected = List.of(address1, address2, address3);
 
         assertThat(rsl, is(expected));
     }
 
-    @Test
-    public void whenCollectAddressAndFilterHomeTwo() {
-        Address address1 = new Address("KZN", "Mira", 20, 235);
-        Address address2 = new Address("MSK", "Gagarina", 3, 2);
-        Address address3 = new Address("ORN", "Lenina", 45, 6);
-
-        Profile profile1 = new Profile(address1);
-        Profile profile2 = new Profile(address2);
-        Profile profile3 = new Profile(address3);
-
-        List<Profile> profiles = List.of(profile1, profile2, profile3);
-        Pro pro = new Pro();
-        List<Address> rsl = pro.collect(profiles);
-        List<Address> expected = List.of(address1, address3);
-
-        assertThat(rsl, is(expected));
-    }
 }
