67a924a636f60435215094590e2b7b1a304d900b
Zver <krutaxe@mail.ru>
2021-11-15 21:26:14 +0300

    6. Тестовое задание из модуля коллекции Lite переделать на Stream API. [#24260]

--- src/main/java/ru/job4j/bank/BankService.java
+++ src/main/java/ru/job4j/bank/BankService.java
@@ -47,14 +47,11 @@ public class BankService {
      * @return null, если пользователь не найден.
      */
     public User findByPassport(String passport) {
-        User rsl = null;
-        for (User user : users.keySet()) {
-            if (user.getPassport().equals(passport)) {
-                rsl = user;
-                break;
-            }
-        }
-        return rsl;
+        return users.keySet()
+                .stream()
+                .filter(u -> u.getPassport().equals(passport))
+                .findFirst()
+                .orElse(null);
     }
 
     /**
@@ -68,16 +65,14 @@ public class BankService {
         Account rsl = null;
         User user = findByPassport(passport);
         if (user != null) {
-            List<Account> accounts = users.get(user);
-            for (Account account : accounts) {
-                if (account.getRequisite().equals(requisite)) {
-                    rsl = account;
-                    break;
-                }
-            }
+            return users.get(user)
+                    .stream()
+                    .filter(u -> u.getRequisite().equals(requisite))
+                    .findFirst()
+                    .orElse(null);
         }
 
-        return rsl;
+        return null;
     }
 
     /**
