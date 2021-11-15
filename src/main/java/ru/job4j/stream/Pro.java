ec1fb897dd430ccb0786fdd8a4a530b1d625a9c7
Zver <krutaxe@mail.ru>
2021-11-15 12:58:57 +0300

    3. Уникальность элементов и сортировка. [#110225]

--- src/main/java/ru/job4j/stream/Pro.java
+++ src/main/java/ru/job4j/stream/Pro.java
@@ -1,12 +1,13 @@
 package ru.job4j.stream;
 
+import java.util.Comparator;
 import java.util.List;
 import java.util.stream.Collectors;
 
 public class Pro {
     public List<Address> collect(List<Profile> profiles) {
         return profiles.stream().map(Profile::getAddress)
-                .sorted((x, y) -> x.getCity().compareTo(y.getCity()))
+                .sorted(Comparator.comparing(Address::getCity))
                 .distinct()
                 .collect(Collectors.toList());
     }
