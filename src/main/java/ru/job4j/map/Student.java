2b310b35f8c67fc2700b3f653a7dbee34c4ed0dc
Zver <krutaxe@mail.ru>
2021-11-16 16:26:40 +0300

    3. Optional в банковских переводах. [#214711]

---
+++ src/main/java/ru/job4j/map/Student.java
@@ -0,0 +1,46 @@
+package ru.job4j.map;
+
+import java.util.Objects;
+
+public class Student {
+    private String name;
+
+    private String account;
+
+    private String group;
+
+    public Student(String name, String account, String group) {
+        this.name = name;
+        this.account = account;
+        this.group = group;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getAccount() {
+        return account;
+    }
+
+    public String getGroup() {
+        return group;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        Student student = (Student) o;
+        return Objects.equals(account, student.account);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(account);
+    }
+}
