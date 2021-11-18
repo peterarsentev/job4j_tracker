9404c5061a61d22c4f098ee188ada035a4153ab6
mary <KotovalovaM@yandex.ru>
2021-11-18 20:34:54 +0700

    п

---
+++ src/main/java/ru/job4j/poly/Bus.java
@@ -0,0 +1,18 @@
+package ru.job4j.poly;
+
+public class Bus implements Transport{
+    @Override
+    public void run() {
+        System.out.println("Поехали");
+    }
+
+    @Override
+    public void passengers(int number_passengers) {
+        System.out.println("Число пассажиров");
+    }
+
+    @Override
+    public float refuel(float amount_of_fuel) {
+        return 0;
+    }
+}
---
+++ src/main/java/ru/job4j/poly/Transport.java
@@ -0,0 +1,10 @@
+package ru.job4j.poly;
+
+public interface Transport {
+    void run();
+
+    void passengers(int number_passengers);
+
+    float refuel(float amount_of_fuel);
+
+}
