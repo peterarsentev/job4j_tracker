1a99601ae96496a058887aa06df1e588e2b3bab8
Veronika Lapenok <veronika.lapenok@mail.ru>
2021-11-18 13:55:49 +0300

    1. Телефонный справочник на базе ArrayList [#41598 #229524]

--- src/test/java/ru/job4j/search/PhoneDictionaryTest.java
+++ src/test/java/ru/job4j/search/PhoneDictionaryTest.java
@@ -4,7 +4,7 @@ import org.junit.Test;
 
 import java.util.ArrayList;
 import static org.hamcrest.core.Is.is;
-import static org.junit.Assert.assertThat;
+import static org.junit.Assert.*;
 
 public class PhoneDictionaryTest {
     @Test
@@ -24,5 +24,15 @@ public class PhoneDictionaryTest {
         );
         ArrayList<Person> persons = phones.find("34");
         assertThat(persons.get(0).getSurname(), is("Lapenok"));
+    }
+
+    @Test
+    public void whenKeyNotFound() {
+        PhoneDictionary phones = new PhoneDictionary();
+        phones.add(
+                new Person("Nika", "Lapenok", "12345", "Vilnius")
+        );
+        ArrayList<Person> persons = phones.find("000");
+        assertTrue(persons.isEmpty());
     }
 }
