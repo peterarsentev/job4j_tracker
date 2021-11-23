4ffc3af3aa15da217fc38a4032e734a87149fa94
Veronika Lapenok <veronika.lapenok@mail.ru>
2021-11-23 17:29:56 +0300

    1. Уникальные задачи. [#212653]

---
+++ src/main/java/ru/job4j/collection/FullSearch.java
@@ -0,0 +1,14 @@
+package ru.job4j.collection;
+
+import java.util.HashSet;
+import java.util.List;
+
+public class FullSearch {
+    public static HashSet<String> extractNumber(List<Task> list) {
+        HashSet<String> numbers = new HashSet<>();
+        for (Task task : list) {
+            numbers.add(task.getNumber());
+        }
+        return numbers;
+    }
+}
