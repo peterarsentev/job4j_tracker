0a2ef06f5425e4b75412a1f7aef615254df3c8e1
Veronika Lapenok <veronika.lapenok@mail.ru>
2021-11-24 11:45:38 +0300

    2. Идентичные тексты [#212655 #232258]

--- src/main/java/ru/job4j/collection/UniqueText.java
+++ src/main/java/ru/job4j/collection/UniqueText.java
@@ -14,6 +14,7 @@ public class UniqueText {
         for (String word : text) {
             if (!check.contains(word)) {
                 result = false;
+                break;
             }
         }
         return result;
