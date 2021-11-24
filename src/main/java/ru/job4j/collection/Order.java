064925b78e6f6d43fa98c12407bbd0b0ea3e5e24
Veronika Lapenok <veronika.lapenok@mail.ru>
2021-11-24 12:43:16 +0300

    2. Преобразования List в Map. [#10093]

---
+++ src/main/java/ru/job4j/collection/Order.java
@@ -0,0 +1,36 @@
+package ru.job4j.collection;
+
+import java.util.Objects;
+
+public class Order {
+    private String number;
+    private String name;
+
+    public Order(String number, String name) {
+        this.number = number;
+        this.name = name;
+    }
+
+    public String getNumber() {
+        return number;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        Order order = (Order) o;
+        return Objects.equals(number, order.number)
+                && Objects.equals(name, order.name);
+
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(number, name);
+    }
+}
