177ded81f83c60b6e7a3ac06ee2a5dc8fa40121d
maria <kotovalovam@yandex.ru>
2021-12-08 22:15:48 +0700

    Пользовательские исключения. [#219367]

---
+++ src/main/java/ru/job4j/ex/FindEl.java
@@ -0,0 +1,26 @@
+package ru.job4j.ex;
+
+public class FindEl {
+    public static int indexOf(String[] value, String key) throws ElementNotFoundException {
+        int rsl = -1;
+        for (int i = 0; i < value.length; i++) {
+            if (value[i] == key){
+                rsl = i;
+            }
+        }
+        if (rsl == -1){
+            throw new ElementNotFoundException("key not found");
+        }
+        return rsl;
+    }
+
+    public static void main(String[] args) {
+        try {
+            String[] str = {"Мама","папа"};
+            int k = FindEl.indexOf(str, "sun");
+            System.out.println(k);
+        } catch (ElementNotFoundException e){
+            e.printStackTrace();
+        }
+    }
+}
