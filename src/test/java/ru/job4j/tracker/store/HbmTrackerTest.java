671f8c4816ae44a4759b1f80fef45c03220b63e5
demonick82 <demonick82@gmail.com>
2021-12-14 01:35:25 +0300

    1. Интеграционные тесты с Hqldb [#4746]

--- src/test/java/ru/job4j/tracker/store/HbmTrackerTest.java
+++ src/test/java/ru/job4j/tracker/store/HbmTrackerTest.java
@@ -40,7 +40,6 @@ public class HbmTrackerTest {
         store.delete(1);
         assertThat(store.findAll().size(), is(0));
     }
-
     @Test
     public void whenFindAll() {
         Store store = new HbmTracker();
