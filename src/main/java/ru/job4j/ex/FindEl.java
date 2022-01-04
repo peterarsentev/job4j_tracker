9875dba3bfb8c83b84805ab71e4150d085aa17e0
maria <kotovalovam@yandex.ru>
2022-01-04 20:16:27 +0700

    Иерархия исключений и множественный catch. [#219368]

--- src/main/java/ru/job4j/ex/FindEl.java
+++ src/main/java/ru/job4j/ex/FindEl.java
@@ -3,7 +3,7 @@ package ru.job4j.ex;
 public class FindEl {
     public static int indexOf(String[] value, String key) throws ElementNotFoundException {
         int rsl = -1;
-        for (int i = 0; i < value.length; i++) {
+        for(int i = 0; i < value.length; i++) {
             if (value[i].equals(key)){
                 rsl = i;
                 break;
