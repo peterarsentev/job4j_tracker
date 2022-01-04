00122aef5a1a3d503a8420b97c25681e99401ff2
maria <kotovalovam@yandex.ru>
2022-01-04 19:12:09 +0700

    Тестирование исключений с junit [#219366]

--- src/main/java/ru/job4j/ex/FindEl.java
+++ src/main/java/ru/job4j/ex/FindEl.java
@@ -4,8 +4,9 @@ public class FindEl {
     public static int indexOf(String[] value, String key) throws ElementNotFoundException {
         int rsl = -1;
         for (int i = 0; i < value.length; i++) {
-            if (value[i] == key){
+            if (value[i].equals(key)){
                 rsl = i;
+                break;
             }
         }
         if (rsl == -1){
