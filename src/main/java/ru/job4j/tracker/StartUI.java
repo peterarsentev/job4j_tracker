fbe6e175dfd2a12da322c9da34b7f79f645f46fc
maria <kotovalovam@yandex.ru>
2022-01-05 17:21:50 +0700

    Тесты на StartUI. [#223313]

--- src/main/java/ru/job4j/tracker/StartUI.java
+++ src/main/java/ru/job4j/tracker/StartUI.java
@@ -43,7 +43,7 @@ public class StartUI {
 
     public static void main(String[] args) {
             Output output = new ConsoleOutput();
-            Input input = new ValidateInput();
+            Input input = new ValidateInput(output, new ConsoleInput());
             Tracker tracker = new Tracker();
             UserAction[] actions = {
                     new CreateAction(output), new ShowAll(output), new EditItem(output),
--- src/main/java/ru/job4j/tracker/ValidateInput.java
+++ src/main/java/ru/job4j/tracker/ValidateInput.java
@@ -1,13 +1,27 @@
 package ru.job4j.tracker;
 
-public class ValidateInput extends ConsoleInput {
+public class ValidateInput implements Input {
+
+    private final Output out;
+    private final Input in;
+
+    public ValidateInput(Output out, Input input) {
+        this.out = out;
+        this.in = input;
+    }
+
+    @Override
+    public String askStr(String question) {
+        return in.askStr(question);
+    }
+
     @Override
     public int askInt(String question) {
         boolean invalid = true;
         int value = -1;
         do {
             try {
-                value = super.askInt(question);
+                value = in.askInt(question);
                 invalid = false;
             } catch (NumberFormatException nfe) {
                 System.out.println("Please enter validate data again.");
