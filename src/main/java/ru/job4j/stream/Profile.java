e9b9661f50a56ba1674e47245b8644528ae701ab
Veronika Lapenok <veronika.lapenok@mail.ru>
2022-01-06 17:08:57 +0300

    2. Список адресов [#110062]

---
+++ src/main/java/ru/job4j/stream/Profile.java
@@ -0,0 +1,13 @@
+package ru.job4j.stream;
+
+public class Profile {
+    private Address address;
+
+    public Profile(Address address) {
+        this.address = address;
+    }
+
+    public Address getAddress() {
+        return address;
+    }
+}
---
+++ src/main/java/ru/job4j/stream/Profiles.java
@@ -0,0 +1,12 @@
+package ru.job4j.stream;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class Profiles {
+    public static List<Address> collect(List<Profile> profiles) {
+        return profiles.stream()
+                .map(Profile::getAddress)
+                .collect(Collectors.toList());
+    }
+}
