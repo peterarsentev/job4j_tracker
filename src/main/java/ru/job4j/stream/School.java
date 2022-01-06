cac81e2010a08a61e79d1c54d9f61ed57be74ffc
Veronika Lapenok <veronika.lapenok@mail.ru>
2022-01-06 16:10:22 +0300

    1. Фильтрация учеников [#110058]

---
+++ src/main/java/ru/job4j/stream/School.java
@@ -0,0 +1,13 @@
+package ru.job4j.stream;
+
+import java.util.List;
+import java.util.function.Predicate;
+import java.util.stream.Collectors;
+
+public class School {
+    public List<Student> collect(List<Student> students, Predicate<Student> predicate) {
+        return students.stream()
+                .filter(predicate)
+                .collect(Collectors.toList());
+    }
+}
