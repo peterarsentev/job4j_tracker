7905ad3e4097f3014359babef63130fdfa14d953
maria <kotovalovam@yandex.ru>
2022-01-07 15:57:19 +0700

    final [#334073]

---
+++ src/main/java/ru/job4j/ooa/Airbus.java
@@ -0,0 +1,49 @@
+package ru.job4j.ooa;
+
+public final class Airbus extends Aircraft {
+    private static int countEngine;
+    private static final int COUNT_ENGINE = 2;
+
+    private String name;
+
+    public Airbus(String name) {
+        this.name = name;
+        this.howCountEngine();
+    }
+
+    private int howCountEngine() {
+        if (name.equals("A380")) {
+            countEngine = 4;
+        } else {
+            countEngine = COUNT_ENGINE;
+        }
+        return countEngine;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+        this.howCountEngine();
+    }
+
+    @Override
+    public void printModel() {
+        System.out.println("Модель самолета: " + name);
+    }
+
+    public void printCountEngine() {
+        System.out.println("Количество двигателей равно: " + countEngine);
+    }
+
+    @Override
+    public String toString() {
+        return "Airbus{"
+                + "name='" + name + '\''
+                + ", "
+                + "Количество двигателей равно = " + countEngine + '\''
+                + '}';
+    }
+}
---
+++ src/main/java/ru/job4j/ooa/Aircraft.java
@@ -0,0 +1,7 @@
+package ru.job4j.ooa;
+
+public class Aircraft {
+    public void printModel() {
+        System.out.println("Метод выводит в консоль модель самолета.");
+    }
+}
