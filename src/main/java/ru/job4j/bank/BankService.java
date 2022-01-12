2e60da78c266f1957be41c199a42d4507f969971
Veronika Lapenok <veronika.lapenok@mail.ru>
2022-01-12 14:41:37 +0300

    6. Тестовое задание из модуля коллекции Lite переделать на Stream API. [#24260]

--- src/main/java/ru/job4j/bank/BankService.java
+++ src/main/java/ru/job4j/bank/BankService.java
@@ -49,12 +49,11 @@ public class BankService {
      * передаваемым номером паспорта не в коллекции
      */
     public User findByPassport(String passport) {
-        for (User user: users.keySet()) {
-            if (passport.equals(user.getPassport())) {
-                return user;
-            }
-        }
-        return null;
+        return users.keySet()
+                .stream()
+                .filter(user -> user.getPassport().equals(passport))
+                .findFirst()
+                .orElse(null);
     }
 
     /**
@@ -69,12 +68,11 @@ public class BankService {
     public Account findByRequisite(String passport, String requisite) {
         User user = findByPassport(passport);
         if (user != null) {
-            List<Account> accounts = users.get(user);
-            for (Account account : accounts) {
-                if (requisite.equals(account.getRequisite())) {
-                    return account;
-                }
-            }
+            return users.get(user)
+                    .stream()
+                    .filter(account -> account.getRequisite().equals(requisite))
+                    .findFirst()
+                    .orElse(null);
         }
         return null;
     }
---
+++ src/main/java/ru/job4j/map/Student.java
@@ -0,0 +1,65 @@
+package ru.job4j.map;
+
+import java.util.Objects;
+
+public class Student {
+    private String name;
+    private String account;
+    private String group;
+
+    public Student(String name, String account, String group) {
+        this.name = name;
+        this.account = account;
+        this.group = group;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAccount() {
+        return account;
+    }
+
+    public void setAccount(String account) {
+        this.account = account;
+    }
+
+    public String getGroup() {
+        return group;
+    }
+
+    public void setGroup(String group) {
+        this.group = group;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        Student student = (Student) o;
+        return Objects.equals(account, student.account);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(account);
+    }
+
+    @Override
+    public String toString() {
+        return "Student{"
+                + "name='" + name + '\''
+                + ", account='" + account + '\''
+                + ", group='" + group + '\''
+                + '}';
+    }
+}
