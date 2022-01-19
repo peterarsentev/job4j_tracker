60da1135d4144877cd0795e627ca9a88983bc323
maria <kotovalovam@yandex.ru>
2022-01-19 10:37:30 +0700

    Очередь с приоритетом на LinkedList [#41670]

---
+++ src/main/java/ru/job4j/search/Task.java
@@ -0,0 +1,19 @@
+package ru.job4j.search;
+
+public class Task {
+    private String desc;
+    private int priority;
+
+    public Task(String desc, int priority) {
+        this.desc = desc;
+        this.priority = priority;
+    }
+
+    public String getDesc() {
+        return desc;
+    }
+
+    public int getPriority() {
+        return priority;
+    }
+}
