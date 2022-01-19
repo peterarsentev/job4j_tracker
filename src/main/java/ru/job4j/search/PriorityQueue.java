e24f14a52ebb9a3aea1c7264ce7674de0cff4664
maria <kotovalovam@yandex.ru>
2022-01-19 12:07:40 +0700

    Очередь с приоритетом на LinkedList [#41670 #252756]

--- src/main/java/ru/job4j/search/PriorityQueue.java
+++ src/main/java/ru/job4j/search/PriorityQueue.java
@@ -14,10 +14,10 @@ public class PriorityQueue {
     public void put(Task task) {
         int index = 0;
         for (Task element : tasks) {
-            if (task.getPriority() >= element.getPriority()) {
-                index++;
+            if (task.getPriority() < element.getPriority()) {
+                break;
             }
-            break;
+            index++;
         }
         this.tasks.add(index, task);
     }
