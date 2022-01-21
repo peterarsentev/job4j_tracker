df1ebe6cf2b247ec8cc1f2bf80efdc187c25aa01
maria <kotovalovam@yandex.ru>
2022-01-21 20:37:20 +0700

    1. Уникальные задачи. [#212653]

---
+++ src/main/java/ru/job4j/collection/Task.java
@@ -0,0 +1,15 @@
+package ru.job4j.collection;
+
+public class Task {
+    private String number;
+    private String description;
+
+    public Task(String number, String description) {
+        this.number = number;
+        this.description = description;
+    }
+
+    public String getNumber() {
+        return number;
+    }
+}
