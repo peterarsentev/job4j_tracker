f894533cc3af403222bc27266ed0b5aaf672f2eb
maria <kotovalovam@yandex.ru>
2022-01-27 12:41:31 +0700

    2. Комбинированный компаратор [#10036 #255517]

--- src/test/java/ru/job4j/collection/JobTest.java
+++ src/test/java/ru/job4j/collection/JobTest.java
@@ -57,7 +57,7 @@ public class JobTest {
         Comparator<Job> cmpNamePriority
                 = new JobDescByName().thenComparing(new JobDescByPriority());
         int rsl = cmpNamePriority.compare(
-                new Job("Impl task", 0),
+                new Job("Fix bug", 0),
                 new Job("Fix bug", 1)
         );
         assertThat(rsl, lessThan(0));
