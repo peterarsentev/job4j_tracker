e656ae009b70fa2b56b3bde53b7f6aa7f8b36491
Veronika Lapenok <veronika.lapenok@mail.ru>
2022-01-27 17:46:46 +0300

    2. Local-Variable Type Inference [#70626 #250398]

--- src/test/java/ru/job4j/search/PhoneDictionaryTest.java
+++ src/test/java/ru/job4j/search/PhoneDictionaryTest.java
@@ -9,39 +9,39 @@ import static org.junit.Assert.assertThat;
 public class PhoneDictionaryTest {
     @Test
     public void whenFindByName() {
-        PhoneDictionary phones = new PhoneDictionary();
+        var phones = new PhoneDictionary();
         phones.add(new Person("Petr", "Arsentev", "534872", "Bryansk"));
-        ArrayList<Person> persons = phones.find("Petr");
+        var persons = phones.find("Petr");
         assertThat(persons.get(0).getSurname(), is("Arsentev"));
     }
 
     @Test
     public void whenFindBySurname() {
-        PhoneDictionary phones = new PhoneDictionary();
+        var phones = new PhoneDictionary();
         phones.add(new Person("Petr", "Arsentev", "534872", "Bryansk"));
         phones.add(new Person("Ivan", "Ivanov", "123456", "Moscow"));
         phones.add(new Person("Veronika", "Lapenok", "6707680", "Vilnius"));
-        ArrayList<Person> persons = phones.find("Ivanov");
+        var persons = phones.find("Ivanov");
         assertThat(persons.get(0).getAddress(), is("Moscow"));
     }
 
     @Test
     public void whenFindByPhone() {
-        PhoneDictionary phones = new PhoneDictionary();
+        var phones = new PhoneDictionary();
         phones.add(new Person("Petr", "Arsentev", "534872", "Bryansk"));
         phones.add(new Person("Ivan", "Ivanov", "123456", "Moscow"));
         phones.add(new Person("Veronika", "Lapenok", "6707680", "Vilnius"));
-        ArrayList<Person> persons = phones.find("6707680");
+        var persons = phones.find("6707680");
         assertThat(persons.get(0).getName(), is("Veronika"));
     }
 
     @Test
     public void whenFindByAddress() {
-        PhoneDictionary phones = new PhoneDictionary();
+        var phones = new PhoneDictionary();
         phones.add(new Person("Petr", "Arsentev", "534872", "Bryansk"));
         phones.add(new Person("Ivan", "Ivanov", "123456", "Bryansk"));
         phones.add(new Person("Veronika", "Lapenok", "6707680", "Vilnius"));
-        ArrayList<Person> persons = phones.find("Bryansk");
+        var persons = phones.find("Bryansk");
         assertThat(persons.get(0).getName(), is("Petr"));
         assertThat(persons.get(1).getName(), is("Ivan"));
     }
--- src/test/java/ru/job4j/search/PriorityQueueTest.java
+++ src/test/java/ru/job4j/search/PriorityQueueTest.java
@@ -7,11 +7,11 @@ import static org.junit.Assert.assertThat;
 public class PriorityQueueTest {
     @Test
     public void whenHigherPriority() {
-        PriorityQueue queue = new PriorityQueue();
+        var queue = new PriorityQueue();
         queue.put(new Task("low", 5));
         queue.put(new Task("urgent", 1));
         queue.put(new Task("middle", 3));
-        Task result = queue.take();
+        var result = queue.take();
         assertThat(result.getDesc(), is("urgent"));
     }
 }
