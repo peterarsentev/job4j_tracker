c8970f0b9e6aa1f86f640e914e01eb399459e768
Veronika Lapenok <veronika.lapenok@mail.ru>
2022-02-22 17:32:12 +0300

    2. Аттестация [#49777 #262448]

--- src/main/java/ru/job4j/stream/Analyze.java
+++ src/main/java/ru/job4j/stream/Analyze.java
@@ -50,7 +50,7 @@ public class Analyze {
     public static Tuple bestSubject(Stream<Pupil> stream) {
         return stream
                 .flatMap(subject -> subject.getSubjects().stream())
-                .collect(Collectors.groupingBy(Subject::getName, LinkedHashMap::new,
+                .collect(Collectors.groupingBy(Subject::getName,
                         Collectors.summingDouble(Subject::getScore)))
                 .entrySet()
                 .stream()
