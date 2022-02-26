529333e802162d71d25f43e351bf7b5d091c7f4f
maria <kotovalovam@yandex.ru>
2022-02-26 16:45:02 +0700

    1. Фильтрация учеников [#110058]

---
+++ src/main/java/ru/job4j/stream/Student.java
@@ -0,0 +1,40 @@
+package ru.job4j.stream;
+
+import java.util.Objects;
+
+public class Student {
+    private int score;
+
+    private String surname;
+
+    public Student(int score, String surname) {
+        this.score = score;
+        this.surname = surname;
+    }
+
+    public int getScore() {
+        return score;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        Student student = (Student) o;
+        return score == student.score
+                && Objects.equals(surname, student.surname);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(score, surname);
+    }
+}
