c0ed1484c30c0fdd3b20d4b6a1e20afcdeee3821
maria <kotovalovam@yandex.ru>
2022-02-27 14:51:22 +0700

    1. Фильтрация учеников [#110058 #269655]

--- src/main/java/ru/job4j/stream/School.java
+++ src/main/java/ru/job4j/stream/School.java
@@ -7,6 +7,8 @@ import java.util.stream.Collectors;
 public class School {
     public List<Student> collect(List<Student> students,
                                  Predicate<Student> predict) {
-        return students.stream().filter(predict).collect(Collectors.toList());
+        return students.stream()
+                .filter(predict)
+                .collect(Collectors.toList());
     }
 }
