a64bd24fe49f6eb906118d996c6c7273b042771d
kotovalova <kotovalovam@yandex.ru>
2022-03-17 11:23:16 +0700

    0. Stream API улучшения [#58167 #276016]

--- src/main/java/ru/job4j/stream/StudentLevel.java
+++ src/main/java/ru/job4j/stream/StudentLevel.java
@@ -4,6 +4,7 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 public class StudentLevel {
+
     public static List<Student> levelOf(List<Student> students, int bound) {
         return students.stream()
                 .filter(st -> st != null)
