ad5ee8035da7d67e6b889dd38ed288326cb81733
parsentev <parsentev@yandex.ru>
2023-08-11 09:28:09 +0300

    Fix tests

--- src/test/java/ru/job4j/tracker/TrackerTest.java
+++ src/test/java/ru/job4j/tracker/TrackerTest.java
@@ -1,6 +1,6 @@
 package ru.job4j.tracker;
 
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 public class TrackerTest {
