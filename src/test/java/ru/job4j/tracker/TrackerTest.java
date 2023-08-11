e313b93c6ff1a378ec180dbadc9ef5b9495e7123
parsentev <parsentev@yandex.ru>
2023-08-11 12:41:18 +0300

    fix imports in tests

--- src/test/java/ru/job4j/tracker/TrackerTest.java
+++ src/test/java/ru/job4j/tracker/TrackerTest.java
@@ -1,7 +1,7 @@
 package ru.job4j.tracker;
 
 import org.junit.jupiter.api.Test;
-import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.assertj.core.api.Assertions.assertThat;
 
 public class TrackerTest {
     @Test
